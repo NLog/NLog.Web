068e4d8d2cbdf0a9c2547e98eff5e45a30bc161f
Xharze <kimworking@gmail.com>
2015-01-31 21:55:42 +0100

    Create project and site name layout renderer

---
+++ NLog.Web/IISInstanceNameLayoutRenderer.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+using System.Web.Hosting;
+using NLog.LayoutRenderers;
+
+namespace NLog.Web
+{
+    [LayoutRenderer("iis-site-name")]
+    public class IISInstanceNameLayoutRenderer : LayoutRenderer
+    {
+        protected override void Append(StringBuilder builder, LogEventInfo logEvent)
+        {
+            builder.Append(HostingEnvironment.SiteName);
+        }
+    }
+}
