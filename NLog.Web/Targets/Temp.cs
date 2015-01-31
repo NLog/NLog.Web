c83cd8d6014a87865196f3d9b6da6f091c1114cd
Xharze <kimworking@gmail.com>
2015-01-31 22:40:00 +0100

    Copy AspNetTraceTarget

---
+++ NLog.Web/Targets/Temp.cs
@@ -0,0 +1,42 @@
+﻿using System.Web;
+using NLog.Targets;
+
+namespace NLog.Web.Targets
+{
+    /// <summary>
+    /// Writes log messages to the ASP.NET trace.
+    /// </summary>
+    /// <seealso href="https://github.com/nlog/nlog/wiki/AspNetTrace-target">Documentation on NLog Wiki</seealso>
+    /// <remarks>
+    /// Log entries can then be viewed by navigating to http://server/path/Trace.axd.
+    /// </remarks>
+    [Target("AspNetTrace")]
+    public class AspNetTraceTarget : TargetWithLayout
+    {
+        /// <summary>
+        /// Writes the specified logging event to the ASP.NET Trace facility. 
+        /// If the log level is greater than or equal to <see cref="LogLevel.Warn"/> it uses the
+        /// System.Web.TraceContext.Warn method, otherwise it uses
+        /// System.Web.TraceContext.Write method.
+        /// </summary>
+        /// <param name="logEvent">The logging event.</param>
+        protected override void Write(LogEventInfo logEvent)
+        {
+            HttpContext context = HttpContext.Current;
+
+            if (context == null)
+            {
+                return;
+            }
+
+            if (logEvent.Level >= LogLevel.Warn)
+            {
+                context.Trace.Warn(logEvent.LoggerName, Layout.Render(logEvent));
+            }
+            else
+            {
+                context.Trace.Write(logEvent.LoggerName, Layout.Render(logEvent));
+            }
+        }
+    }
+}
