b5215569c6041c85f5b9c91f227a76829127ffb5
Julian Verdurmen <304NotModified@users.noreply.github.com>
2015-08-31 23:34:15 +0200

    added HttpDiagnosticsContext (HDC)

---
+++ NLog.Web/HttpContextDiagnosticsContext.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Configuration;
+using NLog.Common;
+
+namespace NLog.Web
+{
+    public class HttpDiagnosticsContext
+    {
+ 
+        private static IDictionary Context
+        {
+            get
+            {
+
+                var httpContext = HttpContext.Current;
+                if (httpContext == null)
+                {
+                    InternalLogger.Error("httpContext is NULL");
+                    if (LogManager.ThrowExceptions)
+                    {
+                        throw new NLogRuntimeException("httpContext is NULL");
+                    }
+                    InternalLogger.Warn("httpContext is NULL - fake context will be used");
+
+
+
+                    return new Dictionary<string, object>();
+                }
+
+                return httpContext.Items;
+            }
+        }
+
+        /// <summary>
+        /// Adds an element with the provided key and value to the <see cref="T:System.Collections.IDictionary"/> object.
+        /// </summary>
+        /// <param name="key">The <see cref="T:System.Object"/> to use as the key of the element to add. </param><param name="value">The <see cref="T:System.Object"/> to use as the value of the element to add. </param><exception cref="T:System.ArgumentNullException"><paramref name="key"/> is null. </exception><exception cref="T:System.ArgumentException">An element with the same key already exists in the <see cref="T:System.Collections.IDictionary"/> object. </exception><exception cref="T:System.NotSupportedException">The <see cref="T:System.Collections.IDictionary"/> is read-only.-or- The <see cref="T:System.Collections.IDictionary"/> has a fixed size. </exception>
+        public void Add(string key, object value)
+        {
+            Context.Add(key, value);
+        }
+
+        /// <summary>
+        /// Removes all elements from the <see cref="T:System.Collections.IDictionary"/> object.
+        /// </summary>
+        /// <exception cref="T:System.NotSupportedException">The <see cref="T:System.Collections.IDictionary"/> object is read-only. </exception>
+        public void Clear()
+        {
+            Context.Clear();
+        }
+
+        /// <summary>
+        /// Determines whether the <see cref="T:System.Collections.IDictionary"/> object contains an element with the specified key.
+        /// </summary>
+        /// <returns>
+        /// true if the <see cref="T:System.Collections.IDictionary"/> contains an element with the key; otherwise, false.
+        /// </returns>
+        /// <param name="key">The key to locate in the <see cref="T:System.Collections.IDictionary"/> object.</param><exception cref="T:System.ArgumentNullException"><paramref name="key"/> is null. </exception>
+        public bool Contains(string key)
+        {
+            return Context.Contains(key);
+        }
+
+        /// <summary>
+        /// Returns an <see cref="T:System.Collections.IDictionaryEnumerator"/> object for the <see cref="T:System.Collections.IDictionary"/> object.
+        /// </summary>
+        /// <returns>
+        /// An <see cref="T:System.Collections.IDictionaryEnumerator"/> object for the <see cref="T:System.Collections.IDictionary"/> object.
+        /// </returns>
+        public IDictionaryEnumerator GetEnumerator()
+        {
+            return Context.GetEnumerator();
+        }
+
+        /// <summary>
+        /// Gets an <see cref="T:System.Collections.ICollection"/> object containing the keys of the <see cref="T:System.Collections.IDictionary"/> object.
+        /// </summary>
+        /// <returns>
+        /// An <see cref="T:System.Collections.ICollection"/> object containing the keys of the <see cref="T:System.Collections.IDictionary"/> object.
+        /// </returns>
+        public ICollection Keys
+        {
+            get { return Context.Keys; }
+        }
+
+        /// <summary>
+        /// Removes the element with the specified key from the <see cref="T:System.Collections.IDictionary"/> object.
+        /// </summary>
+        /// <param name="key">The key of the element to remove. </param><exception cref="T:System.ArgumentNullException"><paramref name="key"/> is null. </exception><exception cref="T:System.NotSupportedException">The <see cref="T:System.Collections.IDictionary"/> object is read-only.-or- The <see cref="T:System.Collections.IDictionary"/> has a fixed size. </exception>
+        public void Remove(string key)
+        {
+            Context.Remove(key);
+        }
+
+        /// <summary>
+        /// Gets or sets the element with the specified key.
+        /// </summary>
+        /// <returns>
+        /// The element with the specified key.
+        /// </returns>
+        /// <param name="key">The key of the element to get or set. </param><exception cref="T:System.ArgumentNullException"><paramref name="key"/> is null. </exception><exception cref="T:System.NotSupportedException">The property is set and the <see cref="T:System.Collections.IDictionary"/> object is read-only.-or- The property is set, <paramref name="key"/> does not exist in the collection, and the <see cref="T:System.Collections.IDictionary"/> has a fixed size. </exception>
+        public object this[string key]
+        {
+            get { return Context[key]; }
+            set { Context[key] = value; }
+        }
+
+        public static HttpDiagnosticsContext Current = new HttpDiagnosticsContext();
+    }
+}
