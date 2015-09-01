33ad3d355acc7760feddd12139f20dd6050bf21c
Julian Verdurmen <304NotModified@users.noreply.github.com>
2015-09-02 00:11:08 +0200

    added unit tests

--- NLog.Web.Tests/LayoutRenderers/AspNetHttpDiagnosticsContextLayoutRendererTests.cs
+++ NLog.Web.Tests/LayoutRenderers/AspNetHttpDiagnosticsContextLayoutRendererTests.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Web;
-using System.Web.SessionState;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NLog.LayoutRenderers;
 using NLog.Web.LayoutRenderers;
@@ -12,14 +8,12 @@ using NLog.Web.LayoutRenderers;
 namespace NLog.Web.Tests.LayoutRenderers
 {
     [TestClass()]
-    public class AspNetSessionValueLayoutRendererTests : LayoutRendererTestsBase
+    public class AspNetHttpDiagnosticsContextLayoutRendererTests : LayoutRendererTestsBase
     {
-
-
         [TestMethod()]
         public void SimpleTest()
         {
-            var appSettingLayoutRenderer = new AspNetSessionValueLayoutRenderer()
+            var appSettingLayoutRenderer = new AspNetHttpDiagnosticsContextLayoutRenderer()
             {
                 Variable = "a"
             };
@@ -28,28 +22,14 @@ namespace NLog.Web.Tests.LayoutRenderers
         }
 
         [TestMethod()]
-        public void SimpleTest2()
+        public void MissingTest()
         {
-            var appSettingLayoutRenderer = new AspNetSessionValueLayoutRenderer()
+            var appSettingLayoutRenderer = new AspNetHttpDiagnosticsContextLayoutRenderer()
             {
-                Variable = "a.b"
+                Variable = "X"
             };
 
-            ExecTest("a.b", "c", "c", appSettingLayoutRenderer);
-        }
-
-        [TestMethod()]
-        public void NestedProps()
-        {
-            var appSettingLayoutRenderer = new AspNetSessionValueLayoutRenderer()
-            {
-                Variable = "a.b",
-                EvaluateAsNestedProperties = true
-            };
-
-            var o = new { b = "c" };
-            //set in "a"
-            ExecTest("a", o, "c", appSettingLayoutRenderer);
+            ExecTest("a", "b", "", appSettingLayoutRenderer);
         }
 
         /// <summary>
@@ -61,7 +41,7 @@ namespace NLog.Web.Tests.LayoutRenderers
         /// <param name="appSettingLayoutRenderer"></param>
         private void ExecTest(string key, object value, object expected, LayoutRenderer appSettingLayoutRenderer)
         {
-            Session[key] = value;
+            HttpContextItems[key] = value;
 
             TestValues(expected, appSettingLayoutRenderer);
         }
