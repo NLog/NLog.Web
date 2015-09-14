88b59ab0c8ac964a6b6e859d265857beed10b609
Julian Verdurmen <304NotModified@users.noreply.github.com>
2015-09-14 23:25:41 +0200

    XML doc and cleanup

--- NLog.Web/LayoutRenderers/IISInstanceNameLayoutRenderer.cs
+++ NLog.Web/LayoutRenderers/IISInstanceNameLayoutRenderer.cs
@@ -4,9 +4,18 @@ using NLog.LayoutRenderers;
 
 namespace NLog.Web.LayoutRenderers
 {
+    /// <summary>
+    /// IIS site name - printing <see cref="HostingEnvironment.SiteName"/>
+    /// </summary>
     [LayoutRenderer("iis-site-name")]
+    // ReSharper disable once InconsistentNaming
     public class IISInstanceNameLayoutRenderer : LayoutRenderer
     {
+        /// <summary>
+        /// Append to target
+        /// </summary>
+        /// <param name="builder">The <see cref="StringBuilder"/> to append the rendered data to.</param>
+        /// <param name="logEvent">Logging event.</param>
         protected override void Append(StringBuilder builder, LogEventInfo logEvent)
         {
             builder.Append(HostingEnvironment.SiteName);
