bec447e7ff9c66586b3e5e84a1022acf0774ea9e
Alex Gil <epignosisx@gmail.com>
2016-01-21 21:37:43 -0500

    added unit tests to existing aspnet layout renderers

---
+++ NLog.Web.Tests/LayoutRenderers/AspNetRequestValueLayoutRendererTests.cs
@@ -0,0 +1,256 @@
+﻿using System.Collections.Specialized;
+using System.Web;
+using NLog.Web.LayoutRenderers;
+using NSubstitute;
+using Xunit;
+
+namespace NLog.Web.Tests.LayoutRenderers
+{
+    public class AspNetRequestValueLayoutRendererTests
+    {
+        [Fact]
+        public void NullHttpContextRendersEmptyString()
+        {
+            var renderer = new AspNetRequestValueLayoutRenderer();
+
+            string result = renderer.Render(new LogEventInfo());
+
+            Assert.Empty(result);
+        }
+
+        public class ItemTests
+        {
+            [Fact]
+            public void NullKeyRendersEmptyString()
+            {
+                var httpContext = Substitute.For<HttpContextBase>();
+
+                var renderer = new AspNetRequestValueLayoutRenderer();
+                renderer.HttpContextAccessor = new FakeHttpContextAccessor(httpContext);
+                renderer.Item = null;
+
+                string result = renderer.Render(new LogEventInfo());
+
+                Assert.Empty(result);
+            }
+
+            [Fact]
+            public void KeyNotFoundRendersEmptyString()
+            {
+                var httpContext = Substitute.For<HttpContextBase>();
+
+                var renderer = new AspNetRequestValueLayoutRenderer();
+                renderer.HttpContextAccessor = new FakeHttpContextAccessor(httpContext);
+                renderer.Item = "key";
+
+                string result = renderer.Render(new LogEventInfo());
+
+                Assert.Empty(result);
+            }
+
+            [Fact]
+            public void KeyFoundRendersValue()
+            {
+                var expectedResult = "value";
+                var httpContext = Substitute.For<HttpContextBase>();
+                httpContext.Request["key"].Returns(expectedResult);
+
+                var renderer = new AspNetRequestValueLayoutRenderer();
+                renderer.HttpContextAccessor = new FakeHttpContextAccessor(httpContext);
+                renderer.Item = "key";
+
+                string result = renderer.Render(new LogEventInfo());
+
+                Assert.Equal(expectedResult, result);
+            }
+        }
+
+        public class QueryStringTests
+        {
+            [Fact]
+            public void NullKeyRendersEmptyString()
+            {
+                var httpContext = Substitute.For<HttpContextBase>();
+
+                var renderer = new AspNetRequestValueLayoutRenderer();
+                renderer.HttpContextAccessor = new FakeHttpContextAccessor(httpContext);
+                renderer.QueryString = null;
+
+                string result = renderer.Render(new LogEventInfo());
+
+                Assert.Empty(result);
+            }
+
+            [Fact]
+            public void KeyNotFoundRendersEmptyString()
+            {
+                var httpContext = Substitute.For<HttpContextBase>();
+
+                var renderer = new AspNetRequestValueLayoutRenderer();
+                renderer.HttpContextAccessor = new FakeHttpContextAccessor(httpContext);
+                renderer.QueryString = "key";
+
+                string result = renderer.Render(new LogEventInfo());
+
+                Assert.Empty(result);
+            }
+
+            [Fact]
+            public void KeyFoundRendersValue()
+            {
+                var expectedResult = "value";
+                var httpContext = Substitute.For<HttpContextBase>();
+                httpContext.Request.QueryString.Returns(new NameValueCollection { {"key", expectedResult} });
+
+                var renderer = new AspNetRequestValueLayoutRenderer();
+                renderer.HttpContextAccessor = new FakeHttpContextAccessor(httpContext);
+                renderer.QueryString = "key";
+
+                string result = renderer.Render(new LogEventInfo());
+
+                Assert.Equal(expectedResult, result);
+            }
+        }
+
+        public class FormTests
+        {
+            [Fact]
+            public void NullKeyRendersEmptyString()
+            {
+                var httpContext = Substitute.For<HttpContextBase>();
+
+                var renderer = new AspNetRequestValueLayoutRenderer();
+                renderer.HttpContextAccessor = new FakeHttpContextAccessor(httpContext);
+                renderer.Form = null;
+
+                string result = renderer.Render(new LogEventInfo());
+
+                Assert.Empty(result);
+            }
+
+            [Fact]
+            public void KeyNotFoundRendersEmptyString()
+            {
+                var httpContext = Substitute.For<HttpContextBase>();
+
+                var renderer = new AspNetRequestValueLayoutRenderer();
+                renderer.HttpContextAccessor = new FakeHttpContextAccessor(httpContext);
+                renderer.Form = "key";
+
+                string result = renderer.Render(new LogEventInfo());
+
+                Assert.Empty(result);
+            }
+
+            [Fact]
+            public void KeyFoundRendersValue()
+            {
+                var expectedResult = "value";
+                var httpContext = Substitute.For<HttpContextBase>();
+                httpContext.Request.Form.Returns(new NameValueCollection { { "key", expectedResult } });
+
+                var renderer = new AspNetRequestValueLayoutRenderer();
+                renderer.HttpContextAccessor = new FakeHttpContextAccessor(httpContext);
+                renderer.Form = "key";
+
+                string result = renderer.Render(new LogEventInfo());
+
+                Assert.Equal(expectedResult, result);
+            }
+        }
+
+        public class ServerVariablesTests
+        {
+            [Fact]
+            public void NullKeyRendersEmptyString()
+            {
+                var httpContext = Substitute.For<HttpContextBase>();
+
+                var renderer = new AspNetRequestValueLayoutRenderer();
+                renderer.HttpContextAccessor = new FakeHttpContextAccessor(httpContext);
+                renderer.ServerVariable = null;
+
+                string result = renderer.Render(new LogEventInfo());
+
+                Assert.Empty(result);
+            }
+
+            [Fact]
+            public void KeyNotFoundRendersEmptyString()
+            {
+                var httpContext = Substitute.For<HttpContextBase>();
+
+                var renderer = new AspNetRequestValueLayoutRenderer();
+                renderer.HttpContextAccessor = new FakeHttpContextAccessor(httpContext);
+                renderer.ServerVariable = "key";
+
+                string result = renderer.Render(new LogEventInfo());
+
+                Assert.Empty(result);
+            }
+
+            [Fact]
+            public void KeyFoundRendersValue()
+            {
+                var expectedResult = "value";
+                var httpContext = Substitute.For<HttpContextBase>();
+                httpContext.Request.ServerVariables.Returns(new NameValueCollection { { "key", expectedResult } });
+
+                var renderer = new AspNetRequestValueLayoutRenderer();
+                renderer.HttpContextAccessor = new FakeHttpContextAccessor(httpContext);
+                renderer.ServerVariable = "key";
+
+                string result = renderer.Render(new LogEventInfo());
+
+                Assert.Equal(expectedResult, result);
+            }
+        }
+
+        public class CookieTests
+        {
+            [Fact]
+            public void NullKeyRendersEmptyString()
+            {
+                var httpContext = Substitute.For<HttpContextBase>();
+
+                var renderer = new AspNetRequestValueLayoutRenderer();
+                renderer.HttpContextAccessor = new FakeHttpContextAccessor(httpContext);
+                renderer.Cookie = null;
+
+                string result = renderer.Render(new LogEventInfo());
+
+                Assert.Empty(result);
+            }
+
+            [Fact]
+            public void KeyNotFoundRendersEmptyString()
+            {
+                var httpContext = Substitute.For<HttpContextBase>();
+
+                var renderer = new AspNetRequestValueLayoutRenderer();
+                renderer.HttpContextAccessor = new FakeHttpContextAccessor(httpContext);
+                renderer.Cookie = "key";
+
+                string result = renderer.Render(new LogEventInfo());
+
+                Assert.Empty(result);
+            }
+
+            [Fact]
+            public void KeyFoundRendersValue()
+            {
+                var expectedResult = "value";
+                var httpContext = Substitute.For<HttpContextBase>();
+                httpContext.Request.Cookies.Returns(new HttpCookieCollection {new HttpCookie("key", expectedResult) });
+
+                var renderer = new AspNetRequestValueLayoutRenderer();
+                renderer.HttpContextAccessor = new FakeHttpContextAccessor(httpContext);
+                renderer.Cookie = "key";
+
+                string result = renderer.Render(new LogEventInfo());
+
+                Assert.Equal(expectedResult, result);
+            }
+        }
+    }
+}
---
+++ NLog.Web.Tests/LayoutRenderers/AspNetUserAuthTypeLayoutRendererTests.cs
@@ -0,0 +1,69 @@
+﻿using System.Security.Principal;
+using System.Web;
+using NLog.Web.LayoutRenderers;
+using NSubstitute;
+using Xunit;
+
+namespace NLog.Web.Tests.LayoutRenderers
+{
+    public class AspNetUserAuthTypeLayoutRendererTests
+    {
+        [Fact]
+        public void NullHttpContextRendersEmptyString()
+        {
+            var renderer = new AspNetUserAuthTypeLayoutRenderer();
+
+            string result = renderer.Render(new LogEventInfo());
+
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public void NullUserIdentityRendersEmptyString()
+        {
+            var httpContext = Substitute.For<HttpContextBase>();
+            httpContext.User.Identity.Returns(null as IIdentity);
+
+            var renderer = new AspNetUserAuthTypeLayoutRenderer();
+            renderer.HttpContextAccessor = new FakeHttpContextAccessor(httpContext);
+
+            string result = renderer.Render(new LogEventInfo());
+
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public void UnauthenticatedUserRendersEmptyString()
+        {
+            var httpContext = Substitute.For<HttpContextBase>();
+            var identity = Substitute.For<IIdentity>();
+            identity.IsAuthenticated.Returns(false);
+            httpContext.User.Identity.Returns(identity);
+
+            var renderer = new AspNetUserAuthTypeLayoutRenderer();
+            renderer.HttpContextAccessor = new FakeHttpContextAccessor(httpContext);
+
+            string result = renderer.Render(new LogEventInfo());
+
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public void AuthenticatedUserRendersAuthenticationType()
+        {
+            var expectedResult = "value";
+            var httpContext = Substitute.For<HttpContextBase>();
+            var identity = Substitute.For<IIdentity>();
+            identity.IsAuthenticated.Returns(true);
+            identity.AuthenticationType.Returns(expectedResult);
+            httpContext.User.Identity.Returns(identity);
+
+            var renderer = new AspNetUserAuthTypeLayoutRenderer();
+            renderer.HttpContextAccessor = new FakeHttpContextAccessor(httpContext);
+
+            string result = renderer.Render(new LogEventInfo());
+
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
