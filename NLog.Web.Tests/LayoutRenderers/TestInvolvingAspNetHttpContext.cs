9b1d5ea0a872efdfd799b7abc77c269cff7d3fe8
Peter Mounce <peter.mounce@just-eat.com>
2016-02-03 21:02:26 +0000

    Add support for reading header values from request
    These are possible via server variables, but the use of those are discouraged - they're very much a legacy `System.Web` thing at this point.

--- NLog.Web.Tests/LayoutRenderers/TestInvolvingAspNetHttpContext.cs
+++ NLog.Web.Tests/LayoutRenderers/TestInvolvingAspNetHttpContext.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections;
 using System.IO;
+using System.Reflection;
 using System.Web;
 
 namespace NLog.Web.Tests.LayoutRenderers
@@ -24,15 +26,41 @@ namespace NLog.Web.Tests.LayoutRenderers
 
         protected virtual void CleanUp()
         {
-            
         }
 
         protected HttpContext SetupFakeHttpContext()
         {
-            var httpRequest = new HttpRequest("", "http://stackoverflow/", "");
+            var httpRequest = SetUpHttpRequest();
             var stringWriter = new StringWriter();
             var httpResponse = new HttpResponse(stringWriter);
             return new HttpContext(httpRequest, httpResponse);
+        }
+
+        protected virtual HttpRequest SetUpHttpRequest(string query = "")
+        {
+            return new HttpRequest("", "http://stackoverflow/", query);
+        }
+
+        protected void AddHeader(HttpRequest request, string headerName, string headerValue)
+        {
+            // thanks http://stackoverflow.com/a/13307238
+            var headers = request.Headers;
+            var t = headers.GetType();
+            var item = new ArrayList();
+
+            t.InvokeMember("MakeReadWrite", BindingFlags.InvokeMethod | BindingFlags.NonPublic | BindingFlags.Instance,
+                null,
+                headers, null);
+            t.InvokeMember("InvalidateCachedArrays",
+                BindingFlags.InvokeMethod | BindingFlags.NonPublic | BindingFlags.Instance,
+                null, headers, null);
+            item.Add(headerValue);
+            t.InvokeMember("BaseAdd", BindingFlags.InvokeMethod | BindingFlags.NonPublic | BindingFlags.Instance, null,
+                headers,
+                new object[] {headerName, item});
+            t.InvokeMember("MakeReadOnly", BindingFlags.InvokeMethod | BindingFlags.NonPublic | BindingFlags.Instance,
+                null,
+                headers, null);
         }
     }
 }
