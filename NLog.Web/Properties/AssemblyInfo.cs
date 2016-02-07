9f68a4671e0a66c4e757265049487a4cb3bf47c1
Julian Verdurmen <304NotModified@users.noreply.github.com>
2016-02-08 00:02:58 +0100

    Update AssemblyInfo.cs

--- NLog.Web/Properties/AssemblyInfo.cs
+++ NLog.Web/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("NLog")]
 [assembly: AssemblyProduct("NLog.Web")]
-[assembly: AssemblyCopyright("Copyright © NLog 2015")]
+[assembly: AssemblyCopyright("Copyright © NLog 2015-2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
