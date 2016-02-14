765c055c984b9e7ef9c15d2d0179b3a4fbb3150b
Julian Verdurmen <304NotModified@users.noreply.github.com>
2016-02-15 00:38:41 +0100

    strong name

---
+++ NLog.Web.ASPNET5/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NLog.Web.ASPNET5")]
+[assembly: AssemblyProduct("NLog.Web for ASP.NET5")]
+
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+
+[assembly: AssemblyCopyright("Copyright © NLog 2015-2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("4.0.0.0")] //fixed
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("74d5915b-bea9-404c-b4d0-b663164def37")]
