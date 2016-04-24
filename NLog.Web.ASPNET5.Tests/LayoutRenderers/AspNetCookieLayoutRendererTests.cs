730389792d227788afc8a3a11bf3cb34cf5ba81e
Sreenath <sriren6029@gmail.com>
2016-04-24 09:04:08 -0400

    aspnet-request-cookie Renderer
    aspnet-request-cookie Renderer

---
+++ NLog.Web.ASPNET5.Tests/LayoutRenderers/AspNetCookieLayoutRendererTests.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Web;
+using NLog.Web.LayoutRenderers;
+using NSubstitute;
+using Xunit;
+
+namespace NLog.Web.Tests.LayoutRenderers
+{
+    public class AspNetCookieLayoutRendererTests
+    {
+        public class CookieTests
+        {
+            [Fact]
+            public void NullKeyRendersEmptyString()
+            {
+                var httpContext = Substitute.For<HttpContextBase>();
+
+                var renderer = new AspNetCookieLayoutRenderer();
+                renderer.HttpContextAccessor = new FakeHttpContextAccessor(httpContext);
+                renderer.CookiesNames = null;
+
+                string result = renderer.Render(new LogEventInfo());
+
+                Assert.Empty(result);
+            }
+
+            [Fact]
+            public void KeyNotFoundRendersEmptyString_Flat_Formatting()
+            {
+                var httpContext = Substitute.For<HttpContextBase>();
+                httpContext.Request.Cookies.Returns(new HttpCookieCollection { new HttpCookie("key1", "TEST") });
+                var renderer = new AspNetCookieLayoutRenderer();
+                renderer.HttpContextAccessor = new FakeHttpContextAccessor(httpContext);
+                renderer.CookiesNames = "key";
+                renderer.CookiesNames = "FLAT";
+
+                string result = renderer.Render(new LogEventInfo());
+
+                Assert.Empty(result);
+            }
+
+            [Fact]
+            public void KeyNotFoundRendersEmptyString_Json_Formatting()
+            {
+                var httpContext = Substitute.For<HttpContextBase>();
+                httpContext.Request.Cookies.Returns(new HttpCookieCollection { new HttpCookie("key1", "TEST") });
+                var renderer = new AspNetCookieLayoutRenderer();
+                renderer.HttpContextAccessor = new FakeHttpContextAccessor(httpContext);
+                renderer.CookiesNames = "key";
+                renderer.OutputFormat = "JSON";
+
+                string result = renderer.Render(new LogEventInfo());
+
+                Assert.Empty(result);
+            }
+
+            [Fact]
+            public void KeyFoundRendersValue_Cookie_Mulitple_Items_Flat_Formatting()
+            {
+                var expectedResult = "key=TEST&Key1=TEST1";
+                var httpContext = Substitute.For<HttpContextBase>();
+                var cookie = new HttpCookie("key", "TEST");
+                cookie["Key1"] = "TEST1";
+                var cookies = new HttpCookieCollection();
+                cookies.Add(cookie);
+                httpContext.Request.Cookies.Returns(cookies);
+
+                var renderer = new AspNetCookieLayoutRenderer();
+                renderer.HttpContextAccessor = new FakeHttpContextAccessor(httpContext);
+                renderer.CookiesNames = "key";
+
+                string result = renderer.Render(new LogEventInfo());
+
+                Assert.Equal(expectedResult, result);
+            }
+
+            [Fact]
+            public void KeyFoundRendersValue_Single_Item_Flat_Formatting()
+            {
+                var expectedResult = "key=TEST";
+                var httpContext = Substitute.For<HttpContextBase>();
+                var cookie = new HttpCookie("key", "TEST");
+                var cookies = new HttpCookieCollection();
+                cookies.Add(cookie);
+                httpContext.Request.Cookies.Returns(cookies);
+
+                var renderer = new AspNetCookieLayoutRenderer();
+                renderer.HttpContextAccessor = new FakeHttpContextAccessor(httpContext);
+                renderer.CookiesNames = "key";
+
+                string result = renderer.Render(new LogEventInfo());
+
+                Assert.Equal(expectedResult, result);
+            }
+
+            [Fact]
+            public void KeyFoundRendersValue_Cookie_Mulitple_Items_Json_Formatting()
+            {
+                var expectedResult = "{\"key=TEST&Key1=TEST1\"}";
+                var httpContext = Substitute.For<HttpContextBase>();
+                var cookie = new HttpCookie("key", "TEST");
+                cookie["Key1"] = "TEST1";
+                var cookies = new HttpCookieCollection();
+                cookies.Add(cookie);
+                httpContext.Request.Cookies.Returns(cookies);
+
+                var renderer = new AspNetCookieLayoutRenderer();
+                renderer.HttpContextAccessor = new FakeHttpContextAccessor(httpContext);
+                renderer.CookiesNames = "key";
+                renderer.OutputFormat = "Json";
+
+                string result = renderer.Render(new LogEventInfo());
+
+                Assert.Equal(expectedResult, result);
+            }
+
+            [Fact]
+            public void KeyFoundRendersVakue_Cookie_Mulitple_Cookies_Cookie_Items_Flat_Formatting()
+            {
+                var expectedResult = "key=TEST&Key1=TEST1,key2=Test&key3=Test456";
+                var httpContext = Substitute.For<HttpContextBase>();
+                var cookie = new HttpCookie("key", "TEST");
+                cookie["Key1"] = "TEST1";
+                var cookies = new HttpCookieCollection();
+                cookies.Add(cookie);
+
+                cookie = new HttpCookie("key2", "Test");
+                cookie["key3"] = "Test456";
+                cookies.Add(cookie);
+
+                httpContext.Request.Cookies.Returns(cookies);
+
+                var renderer = new AspNetCookieLayoutRenderer();
+                renderer.HttpContextAccessor = new FakeHttpContextAccessor(httpContext);
+                renderer.CookiesNames = "key, key2";
+
+                string result = renderer.Render(new LogEventInfo());
+
+                Assert.Equal(expectedResult, result);
+            }
+
+            [Fact]
+            public void KeyFoundRendersVakue_Cookie_Mulitple_Cookies_Cookie_Items_Json_Formatting()
+            {
+                var expectedResult = "{\"key=TEST&Key1=TEST1\"},{\"key2=Test&key3=Test456\"}";
+                var httpContext = Substitute.For<HttpContextBase>();
+                var cookie = new HttpCookie("key", "TEST");
+                cookie["Key1"] = "TEST1";
+                var cookies = new HttpCookieCollection();
+                cookies.Add(cookie);
+
+                cookie = new HttpCookie("key2", "Test");
+                cookie["key3"] = "Test456";
+                cookies.Add(cookie);
+
+                httpContext.Request.Cookies.Returns(cookies);
+
+                var renderer = new AspNetCookieLayoutRenderer();
+                renderer.HttpContextAccessor = new FakeHttpContextAccessor(httpContext);
+                renderer.CookiesNames = "key, key2";
+                renderer.OutputFormat = "json";
+
+                string result = renderer.Render(new LogEventInfo());
+
+                Assert.Equal(expectedResult, result);
+            }
+        }
+    }
+}
---
+++ NLog.Web.ASPNET5/LayoutRenderers/AspNetCookieLayoutRenderer.cs
@@ -0,0 +1,124 @@
+﻿using System.Text;
+#if !DNX
+using System.Web;
+using System.Collections.Specialized;
+#else
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using Microsoft.Extensions.Primitives;
+#endif
+using NLog.LayoutRenderers;
+using System.Collections.Generic;
+using NLog.Config;
+using System;
+
+
+
+namespace NLog.Web.LayoutRenderers
+{
+    /// <summary>
+    /// ASP.NET Request Cookie
+    /// </summary>
+    [LayoutRenderer("aspnet-request-cookie")]
+    public class AspNetCookieLayoutRenderer : AspNetLayoutRendererBase
+    {
+        private static string doubleQuotes = "\"";
+        private static string jsonStartBraces = "{";
+        private static string jsonEndBraces = "}";
+        private static string jsonElementSeparator = ",";
+
+        private static string flatCookiesSeparator = "=";
+        private static string flatItemSeperator = ",";
+
+
+        /// <summary>
+        /// Comma separated string of name of the cookies to rendered from Request.
+        /// </summary>
+        public string CookiesNames { get; set; }
+
+        /// <summary>
+        /// Determines how the output is rendered. Possible Value: FLAT, JSON. Default is FLAT.
+        /// </summary>
+        [DefaultParameter]
+        public string OutputFormat { get; set; } = "FLAT";
+
+        /// <summary>
+        /// Renders the ASP.NET Session ID appends it to the specified <see cref="StringBuilder" />.
+        /// </summary>
+        /// <param name="builder">The <see cref="StringBuilder"/> to append the rendered data to.</param>
+        /// <param name="logEvent">Logging event.</param>
+        protected override void DoAppend(StringBuilder builder, LogEventInfo logEvent)
+        {
+            if (this.CookiesNames != null)
+            {
+                var items = this.CookiesNames.Split(new String[] { ",", " " }, StringSplitOptions.RemoveEmptyEntries);
+
+                if (items.Length > 0)
+                {
+                    var httpRequest = HttpContextAccessor.HttpContext.Request;
+
+                    if (httpRequest?.Cookies?.Count == 0)
+                        return;
+
+                    for (int i = 0; i < items.Length; i++)
+                    {
+#if !DNX
+                        var cookie = httpRequest.Cookies[items[i]];
+                        this.SerializeCookie(cookie, builder, i);
+
+#else
+                        var cookie = httpRequest.Cookies[items[i]];
+                        builder.Append(cookie);
+#endif
+                    }
+                }
+            }
+        }
+
+#if !DNX
+        private void SerializeCookie(HttpCookie cookie, StringBuilder builder, int index)
+        {
+            if (cookie != null)
+            {
+                if (this.OutputFormat?.ToUpperInvariant() == "JSON")
+                {
+                    if (index > 0)
+                        builder.Append($"{jsonElementSeparator}");
+
+                    builder.Append($"{jsonStartBraces}{doubleQuotes}{cookie.Name}{flatCookiesSeparator}{cookie.Value}{doubleQuotes}{jsonEndBraces}");
+
+                }
+                else
+                {
+                    if (index > 0)
+                        builder.Append($"{flatItemSeperator}");
+
+                    builder.Append($"{cookie.Name}{flatCookiesSeparator}{cookie.Value}");
+                }
+            }
+        }
+#endif
+
+#if DNX
+        private void SerializeCookie(StringValues cookie, StringBuilder builder, int index)
+        {
+
+            if (this.OutputFormat?.ToUpperInvariant() == "JSON")
+            {
+                if (index > 0)
+                    builder.Append($"{jsonElementSeparator}");
+
+                builder.Append($"{jsonStartBraces}{doubleQuotes}{cookie}{doubleQuotes}{jsonEndBraces}");
+
+            }
+            else
+            {
+                if (index > 0)
+                    builder.Append($"{flatItemSeperator}");
+
+                builder.Append($"{cookie}");
+            }
+        }
+#endif
+    }
+}
