5a41374a414965fda9898220cbaf8406c181fa44
Sreenath <sriren6029@gmail.com>
2016-04-27 20:50:43 -0400

    aspnet-request-referrer Renderer.
    aspnet-request-referrer Renderer. The ASP.NET MVC CORE Version is not
    yet tested.

---
+++ NLog.Web.ASPNET5.Tests/LayoutRenderers/AspNetRequestReferrerRendererTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Web;
+using NLog.Web.LayoutRenderers;
+using NSubstitute;
+using Xunit;
+
+namespace NLog.Web.Tests.LayoutRenderers
+{
+    public class AspNetRequestReferrerRendererTests
+    {
+
+        [Fact]
+        public void NullReferrerRendersEmptyString()
+        {
+            var httpContext = Substitute.For<HttpContextBase>();
+
+            var renderer = new AspNetRequestReferrerRenderer();
+            renderer.HttpContextAccessor = new FakeHttpContextAccessor(httpContext);
+
+            string result = renderer.Render(new LogEventInfo());
+
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public void ReferrerPresentRenderNonEmptyString()
+        {
+            var httpContext = Substitute.For<HttpContextBase>();
+            httpContext.Request.UrlReferrer.Returns(new Uri("http://www.google.com/"));
+            var renderer = new AspNetRequestReferrerRenderer();
+            renderer.HttpContextAccessor = new FakeHttpContextAccessor(httpContext);
+            
+            string result = renderer.Render(new LogEventInfo());
+
+            Assert.Equal(result, "http://www.google.com/");
+        }        
+    }
+}
---
+++ NLog.Web.ASPNET5/LayoutRenderers/AspNetRequestReferrerRenderer.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+#if !DNX
+using System.Web;
+using System.Collections.Specialized;
+#else
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using Microsoft.Extensions.Primitives;
+#endif
+using NLog.LayoutRenderers;
+using System.Collections.Generic;
+using NLog.Config;
+using System;
+
+namespace NLog.Web.LayoutRenderers
+{
+    /// <summary>
+    /// ASP.NET Request Cookie
+    /// </summary>
+    [LayoutRenderer("aspnet-request-referrer")]
+    public class AspNetRequestReferrerRenderer : AspNetLayoutRendererBase
+    {
+        /// <summary>
+        /// Renders the Referrer URL from the HttpRequest
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="logEvent"></param>
+        protected override void DoAppend(StringBuilder builder, LogEventInfo logEvent)
+        {
+            var httpRequest = HttpContextAccessor.HttpContext.Request;
+
+            if (httpRequest == null)
+            {
+                return;
+            }
+
+#if !DNX
+            if (httpRequest.UrlReferrer != null)
+                builder.Append(httpRequest.UrlReferrer.ToString());
+#else
+            builder.Append(HttpContextAccessor.HttpContext.Request.Headers["Referrer"]);
+#endif
+
+        }
+    }
+}
