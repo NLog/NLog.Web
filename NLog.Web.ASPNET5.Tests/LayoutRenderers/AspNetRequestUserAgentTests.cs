11d1ad39e3b30741e01e7f8b772b152fec3999c3
Sreenath <sriren6029@gmail.com>
2016-04-29 22:02:24 -0400

    aspnet-useragent
    aspnet-useragent

---
+++ NLog.Web.ASPNET5.Tests/LayoutRenderers/AspNetRequestUserAgentTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Web;
+using NLog.Web.LayoutRenderers;
+using NSubstitute;
+using Xunit;
+
+namespace NLog.Web.Tests.LayoutRenderers
+{
+    public class AspNetRequestUserAgentTests
+    {
+        [Fact]
+        public void NullUserAgentRendersEmptyString()
+        {
+            var httpContext = Substitute.For<HttpContextBase>();
+
+            var renderer = new AspNetRequestUserAgent();
+            renderer.HttpContextAccessor = new FakeHttpContextAccessor(httpContext);
+
+            string result = renderer.Render(new LogEventInfo());
+
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public void NotNullUserAgentRendersEmptyString()
+        {
+            var httpContext = Substitute.For<HttpContextBase>();
+            httpContext.Request.UserAgent.Returns("TEST");
+            var renderer = new AspNetRequestUserAgent();
+            renderer.HttpContextAccessor = new FakeHttpContextAccessor(httpContext);
+
+            string result = renderer.Render(new LogEventInfo());
+
+            Assert.Equal(result, "TEST");
+        }
+    }
+}
---
+++ NLog.Web.ASPNET5/LayoutRenderers/AspNetRequestuseragent.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+#if !DNX
+using System.Web;
+using System.Collections.Specialized;
+#else
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using Microsoft.Extensions.Primitives;
+#endif
+using NLog.LayoutRenderers;
+using System.Collections.Generic;
+using NLog.Config;
+using System;
+
+namespace NLog.Web.LayoutRenderers
+{
+    /// <summary>
+    /// ASP.NET User Agent
+    /// </summary>
+    /// <example>
+    /// <code lang="NLog Layout Renderer">
+    /// ${aspnet-useragent} - Produces - User Agent String from the Request.
+    /// </code>
+    /// </example>
+    [LayoutRenderer("aspnet-useragent")]
+    public class AspNetRequestUserAgent : AspNetLayoutRendererBase
+    {
+        /// <summary>
+        /// Renders the ASP.NET User Agent
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="logEvent"></param>
+        protected override void DoAppend(StringBuilder builder, LogEventInfo logEvent)
+        {
+            var httpRequest = HttpContextAccessor.HttpContext.Request;
+
+            if (httpRequest == null)
+            {
+                return;
+            }
+#if !DNX
+            if(httpRequest.UserAgent != null)
+            {
+                builder.Append(httpRequest.UserAgent);
+            }
+
+#else
+            builder.Append(httpRequest.Headers["User-Agent"].ToString());
+#endif
+
+        }
+    }
+}
