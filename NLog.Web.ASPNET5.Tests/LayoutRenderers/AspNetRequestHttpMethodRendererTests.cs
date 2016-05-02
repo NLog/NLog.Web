66fffc163784ad97ef911441b391ed24ac9b8388
Sreenath <sriren6029@gmail.com>
2016-05-01 20:39:54 -0400

    aspnet-request-method
    aspnet-request-method

---
+++ NLog.Web.ASPNET5.Tests/LayoutRenderers/AspNetRequestHttpMethodRendererTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Web;
+using NLog.Web.LayoutRenderers;
+using NSubstitute;
+using Xunit;
+
+namespace NLog.Web.Tests.LayoutRenderers
+{
+    public class AspNetRequestHttpMethodRendererTests
+    {
+        [Fact]
+        public void NullUrlRendersEmptyString()
+        {
+            var httpContext = Substitute.For<HttpContextBase>();
+            
+            var renderer = new AspNetRequestHttpMethodRenderer();
+            renderer.HttpContextAccessor = new FakeHttpContextAccessor(httpContext);
+            
+
+            string result = renderer.Render(new LogEventInfo());
+
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public void HttpMethod_Set_Renderer()
+        {
+            var httpContext = Substitute.For<HttpContextBase>();
+            httpContext.Request.HttpMethod.Returns("POST");
+
+            var renderer = new AspNetRequestHttpMethodRenderer();
+            renderer.HttpContextAccessor = new FakeHttpContextAccessor(httpContext);
+
+
+            string result = renderer.Render(new LogEventInfo());
+
+            Assert.Equal("POST", result);
+        }
+    }
+}
---
+++ NLog.Web.ASPNET5/LayoutRenderers/AspNetRequestHttpMethodRenderer.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+#if !DNX
+using System.Web;
+using System.Collections.Specialized;
+#else
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using Microsoft.Extensions.Primitives;
+#endif
+using NLog.LayoutRenderers;
+using System.Collections.Generic;
+using NLog.Config;
+using System;
+
+namespace NLog.Web.LayoutRenderers
+{
+    /// <summary>
+    /// ASP.NET Http Request Method.
+    /// </summary>
+    /// <para>Example usage of ${aspnet-request-method}:</para>
+    /// <example>
+    /// <code lang="NLog Layout Renderer">
+    /// ${aspnet-request-method} - Produces - Post.
+    /// </code>
+    /// </example>
+    [LayoutRenderer("aspnet-request-method")]
+    public class AspNetRequestHttpMethodRenderer : AspNetLayoutRendererBase
+    {
+        /// <summary>
+        /// ASP.NET Http Request Method
+        /// </summary>
+        /// <param name="builder">The <see cref="StringBuilder"/> to append the rendered data to.</param>
+        /// <param name="logEvent">Logging event.</param>
+        protected override void DoAppend(StringBuilder builder, LogEventInfo logEvent)
+        {
+            var httpRequest = HttpContextAccessor?.HttpContext?.Request;
+
+            if (httpRequest == null)
+            {
+                return;
+            }
+
+            string httpMethod = string.Empty;
+#if !DNX
+            httpMethod = httpRequest.HttpMethod;
+
+#else
+            httpMethod = httpRequest.Method;
+#endif
+
+            builder.Append(httpMethod);
+
+        }
+    }
+}
