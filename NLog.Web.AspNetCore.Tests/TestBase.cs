5dc4f9d6a839324cefdd2b46fc5aaf040c7410f2
Julian Verdurmen <304NotModified@users.noreply.github.com>
2016-06-09 00:10:02 +0200

    Move testbase

---
+++ NLog.Web.AspNetCore.Tests/TestBase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+#if NETSTANDARD_1plus
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+#endif
+
+namespace NLog.Web.Tests.LayoutRenderers
+{
+    public class TestBase
+    {
+        /// <summary>Initializes a new instance of the <see cref="T:System.Object" /> class.</summary>
+        public TestBase()
+        {
+            LogManager.ThrowExceptions = true;
+        }
+
+#if NETSTANDARD_1plus
+        protected class HeaderDict : Dictionary<string, StringValues>, IHeaderDictionary
+        {
+            /// <summary>Initializes a new instance of the <see cref="T:System.Collections.Generic.Dictionary`2" /> class that is empty, has the default initial capacity, and uses the default equality comparer for the key type.</summary>
+            public HeaderDict()
+            {
+            }
+
+
+        }
+#endif
+    }
+}
