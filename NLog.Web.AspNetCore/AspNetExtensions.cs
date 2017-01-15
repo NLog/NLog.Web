8b8a8b38e304b7450a806136927012358af40da4
Julian Verdurmen <304NotModified@users.noreply.github.com>
2017-01-15 17:05:28 +0100

    move from nlog.extensions.logging, reference  nlog.extensions.logging & asp.net-core

--- NLog.Web.AspNetCore/AspNetExtensions.cs
+++ NLog.Web.AspNetCore/AspNetExtensions.cs
@@ -1,5 +1,13 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog.Config;
 using NLog.Web.Internal;
+using NLog.Extensions.Logging;
+
 
 namespace NLog.Web
 {
@@ -8,14 +16,42 @@ namespace NLog.Web
     /// </summary>
     public static class AspNetExtensions
     {
+
+
         /// <summary>
-        /// Enable NLog Web for ASP.NET 5.
+        /// Enable NLog Web for ASP.NET Core.
         /// </summary>
         /// <param name="app"></param>
 
         public static void AddNLogWeb(this IApplicationBuilder app)
         {
-            ServiceLocator.ServiceProvider = app.ApplicationServices;            
+            ServiceLocator.ServiceProvider = app.ApplicationServices;
         }
+
+        /// <summary>
+        /// Apply NLog configuration from XML config.
+        /// </summary>
+        /// <param name="env"></param>
+        /// <param name="configFileRelativePath">relative path to NLog configuration file.</param>
+        /// <returns>LoggingConfiguration for chaining</returns>
+        public static LoggingConfiguration ConfigureNLog(this IHostingEnvironment env, string configFileRelativePath)
+        {
+            var fileName = Path.Combine(env.ContentRootPath, configFileRelativePath);
+            return ConfigureNLog(fileName);
+        }
+
+        /// <summary>
+        /// Apply NLog configuration from XML config.
+        /// </summary>
+        /// <param name="fileName">absolute path  NLog configuration file.</param>
+        /// <returns>LoggingConfiguration for chaining</returns>
+        private static LoggingConfiguration ConfigureNLog(string fileName)
+        {
+            var configuration = new XmlLoggingConfiguration(fileName, true);
+            LogManager.Configuration = configuration;
+            return configuration;
+        }
+
+
     }
 }
