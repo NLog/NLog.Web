d0f10efb214bd47b94abdc3eb4035ec20f55ab92
Julian Verdurmen <304NotModified@users.noreply.github.com>
2017-02-10 20:04:13 +0100

    Add ${aspnet-traceidentifier} (ASP.NET Core only)

---
+++ NLog.Web.AspNetCore/LayoutRenderers/Class.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog.LayoutRenderers;
+
+namespace NLog.Web.LayoutRenderers
+{
+    /// <summary>
+    /// Print the TraceIdentifier
+    /// </summary>
+    /// <remarks>.NET Core Only</remarks>
+    [LayoutRenderer("aspnet-traceidentifier")]
+    public class AspNetTraceIdentifierLayoutRenderer : AspNetLayoutRendererBase
+    {
+        /// <inheritdoc />
+        protected override void DoAppend(StringBuilder builder, LogEventInfo logEvent)
+        {
+            var context = HttpContextAccessor.HttpContext;
+            
+            builder.Append(context.TraceIdentifier);
+        }
+    }
+}
