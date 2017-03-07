7fcbde1c1b88ded316f4debcc11eb8558c5ce164
Julian Verdurmen <304NotModified@users.noreply.github.com>
2017-03-07 23:19:05 +0100

    update build script

--- build_aspnetcore.bat
+++ build_aspnetcore.bat
@@ -1,6 +1,4 @@
 echo off
 
-rem update project.json for version number
 
-call dotnet restore NLog.Web.AspNetCore 
-call dotnet pack NLog.Web.AspNetCore --configuration release 
+msbuild NLog.Web.AspNetCore /t:restore /t:build /t:pack /verbosity:minimal /p:configuration=release
