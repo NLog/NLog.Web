ac175deea29a0f84d9c843a93839bde33acbfae4
Julian Verdurmen <304NotModified@users.noreply.github.com>
2017-03-31 00:01:13 +0200

    Add ${aspnet-request-contenttype} (ASP.NET Core only)

---
+++ NLog.Web.AspNetCore/LayoutRenderers/AspNetRequestContentTypeLayoutRenderer.cs
@@ -0,0 +1,39 @@
+#if NETSTANDARD_1plus
+using NLog.LayoutRenderers;
+using System.Text;
+
+using Microsoft.AspNetCore.Routing;
+
+using NLog.Web.Internal;
+
+namespace NLog.Web.LayoutRenderers
+{
+    /// <summary>
+    /// ASP.NET content type.
+    /// </summary>
+    /// <example>
+    /// <code lang="NLog Layout Renderer">
+    /// ${aspnet-request-contenttype}    
+    /// </code>
+    /// </example>
+    [LayoutRenderer("aspnet-request-contenttype")]
+    public class AspNetRequestContentTypeLayoutRenderer : AspNetLayoutRendererBase
+    {
+        /// <summary>
+        /// Renders the specified ASP.NET Application variable and appends it to the specified <see cref="StringBuilder" />.
+        /// </summary>
+        /// <param name="builder">The <see cref="StringBuilder"/> to append the rendered data to.</param>
+        /// <param name="logEvent">Logging event.</param>
+        protected override void DoAppend(StringBuilder builder, LogEventInfo logEvent)
+        {
+            var request = HttpContextAccessor?.HttpContext?.TryGetRequest();
+
+            var contentType = request?.ContentType;
+
+            if (!string.IsNullOrEmpty(contentType))
+                    builder.Append(contentType);
+            
+        }
+    }
+}
+#endif
