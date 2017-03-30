850f4793c05c46ce987b6bff6072028b1bb94d97
grokky1 <grokky1@users.noreply.github.com>
2017-03-30 23:41:26 +0200

    Add aspnet-environment

---
+++ NLog.Web.AspNetCore/LayoutRenderers/AspNetEnvironmentLayoutRenderer.cs
@@ -0,0 +1,31 @@
+﻿#if NETSTANDARD_1plus
+using System;
+using System.Text;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using NLog.LayoutRenderers;
+using NLog.Web.Internal;
+
+namespace NLog.Web.LayoutRenderers
+{
+
+    /// <summary>
+    /// Rendering development environment. <see cref="IHostingEnvironment"/>
+    /// </summary>
+    [LayoutRenderer("aspnet-environment")]
+    // ReSharper disable once InconsistentNaming
+    public class AspNetEnvironmentLayoutRenderer : LayoutRenderer
+    {
+        /// <summary>
+        /// Append to target
+        /// </summary>
+        /// <param name="builder">The <see cref="StringBuilder"/> to append the rendered data to.</param>
+        /// <param name="logEvent">Logging event.</param>
+        protected override void Append(StringBuilder builder, LogEventInfo logEvent)
+        {
+            var env = ServiceLocator.ServiceProvider?.GetService<IHostingEnvironment>();
+            builder.Append(env?.EnvironmentName);
+        }
+    }
+}
+#endif
