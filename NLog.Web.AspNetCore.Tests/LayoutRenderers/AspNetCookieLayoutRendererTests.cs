82a69680dcc443cbe28bec43885a0f103dca2441
Julian Verdurmen <304NotModified@users.noreply.github.com>
2017-04-10 00:04:08 +0200

    remove code dupl in tests

--- NLog.Web.AspNetCore.Tests/LayoutRenderers/AspNetCookieLayoutRendererTests.cs
+++ NLog.Web.AspNetCore.Tests/LayoutRenderers/AspNetCookieLayoutRendererTests.cs
@@ -1,5 +1,4 @@
-﻿
-#if !NETSTANDARD_1plus
+﻿#if !NETSTANDARD_1plus
 //TODO test .NET Core
 using System;
 using System.Collections.Generic;
@@ -31,7 +30,7 @@ namespace NLog.Web.Tests.LayoutRenderers
     {
         public AspNetCookieLayoutRendererTests() : base()
         {
-            
+
         }
 
         [Fact]
@@ -51,27 +50,22 @@ namespace NLog.Web.Tests.LayoutRenderers
         [Fact]
         public void KeyNotFoundRendersEmptyString_Flat_Formatting()
         {
-            var httpContext = Substitute.For<HttpContextBase>();
-            httpContext.Request.Cookies.Returns(new HttpCookieCollection { new HttpCookie("key1", "TEST") });
-            var renderer = new AspNetRequestCookieLayoutRenderer();
-            renderer.HttpContextAccessor = new FakeHttpContextAccessor(httpContext);
-            renderer.CookieNames = new List<string> { "key" };
+            var renderer = CreateRenderer();
             renderer.OutputFormat = AspNetRequestLayoutOutputFormat.Flat;
-
+            renderer.CookieNames = new List<string> { "notfound" };
             string result = renderer.Render(new LogEventInfo());
 
             Assert.Empty(result);
         }
 
+
+
         [Fact]
         public void KeyNotFoundRendersEmptyString_Json_Formatting()
         {
-            var httpContext = Substitute.For<HttpContextBase>();
-            httpContext.Request.Cookies.Returns(new HttpCookieCollection { new HttpCookie("key1", "TEST") });
-            var renderer = new AspNetRequestCookieLayoutRenderer();
-            renderer.HttpContextAccessor = new FakeHttpContextAccessor(httpContext);
-            renderer.CookieNames = new List<string> { "key" };
+            var renderer = CreateRenderer();
             renderer.OutputFormat = AspNetRequestLayoutOutputFormat.Json;
+            renderer.CookieNames = new List<string> { "notfound" };
 
             string result = renderer.Render(new LogEventInfo());
 
@@ -82,16 +76,8 @@ namespace NLog.Web.Tests.LayoutRenderers
         public void KeyFoundRendersValue_Cookie_Mulitple_Items_Flat_Formatting()
         {
             var expectedResult = "key=TEST&Key1=TEST1";
-            var httpContext = Substitute.For<HttpContextBase>();
-            var cookie = new HttpCookie("key", "TEST");
-            cookie["Key1"] = "TEST1";
-            var cookies = new HttpCookieCollection();
-            cookies.Add(cookie);
-            httpContext.Request.Cookies.Returns(cookies);
 
-            var renderer = new AspNetRequestCookieLayoutRenderer();
-            renderer.HttpContextAccessor = new FakeHttpContextAccessor(httpContext);
-            renderer.CookieNames = new List<string> { "key" };
+            var renderer = CreateRenderer();
 
             string result = renderer.Render(new LogEventInfo());
 
@@ -102,15 +88,8 @@ namespace NLog.Web.Tests.LayoutRenderers
         public void KeyFoundRendersValue_Single_Item_Flat_Formatting()
         {
             var expectedResult = "key=TEST";
-            var httpContext = Substitute.For<HttpContextBase>();
-            var cookie = new HttpCookie("key", "TEST");
-            var cookies = new HttpCookieCollection();
-            cookies.Add(cookie);
-            httpContext.Request.Cookies.Returns(cookies);
 
-            var renderer = new AspNetRequestCookieLayoutRenderer();
-            renderer.HttpContextAccessor = new FakeHttpContextAccessor(httpContext);
-            renderer.CookieNames = new List<string> { "key" };
+            var renderer = CreateRenderer(addKey: false);
 
             string result = renderer.Render(new LogEventInfo());
 
@@ -121,16 +100,10 @@ namespace NLog.Web.Tests.LayoutRenderers
         public void KeyFoundRendersValue_Cookie_Mulitple_Items_Json_Formatting()
         {
             var expectedResult = "{\"key=TEST&Key1=TEST1\"}";
-            var httpContext = Substitute.For<HttpContextBase>();
-            var cookie = new HttpCookie("key", "TEST");
-            cookie["Key1"] = "TEST1";
-            var cookies = new HttpCookieCollection();
-            cookies.Add(cookie);
-            httpContext.Request.Cookies.Returns(cookies);
 
-            var renderer = new AspNetRequestCookieLayoutRenderer();
-            renderer.HttpContextAccessor = new FakeHttpContextAccessor(httpContext);
-            renderer.CookieNames = new List<string> { "key" };
+            var renderer = CreateRenderer();
+
+
             renderer.OutputFormat = AspNetRequestLayoutOutputFormat.Json;
 
             string result = renderer.Render(new LogEventInfo());
@@ -142,20 +115,11 @@ namespace NLog.Web.Tests.LayoutRenderers
         public void KeyFoundRendersVakue_Cookie_Mulitple_Cookies_Cookie_Items_Flat_Formatting()
         {
             var expectedResult = "key=TEST&Key1=TEST1,key2=Test&key3=Test456";
-            var httpContext = Substitute.For<HttpContextBase>();
-            var cookie = new HttpCookie("key", "TEST");
-            cookie["Key1"] = "TEST1";
-            var cookies = new HttpCookieCollection();
-            cookies.Add(cookie);
+            var renderer = CreateRenderer(addCookie2: true);
 
-            cookie = new HttpCookie("key2", "Test");
-            cookie["key3"] = "Test456";
-            cookies.Add(cookie);
 
-            httpContext.Request.Cookies.Returns(cookies);
 
-            var renderer = new AspNetRequestCookieLayoutRenderer();
-            renderer.HttpContextAccessor = new FakeHttpContextAccessor(httpContext);
+
             renderer.CookieNames = new List<string> { "key", "key2" };
 
             string result = renderer.Render(new LogEventInfo());
@@ -167,21 +131,7 @@ namespace NLog.Web.Tests.LayoutRenderers
         public void KeyFoundRendersVakue_Cookie_Mulitple_Cookies_Cookie_Items_Json_Formatting()
         {
             var expectedResult = "{\"key=TEST&Key1=TEST1\"},{\"key2=Test&key3=Test456\"}";
-            var httpContext = Substitute.For<HttpContextBase>();
-            var cookie = new HttpCookie("key", "TEST");
-            cookie["Key1"] = "TEST1";
-            var cookies = new HttpCookieCollection();
-            cookies.Add(cookie);
-
-            cookie = new HttpCookie("key2", "Test");
-            cookie["key3"] = "Test456";
-            cookies.Add(cookie);
-
-            httpContext.Request.Cookies.Returns(cookies);
-
-            var renderer = new AspNetRequestCookieLayoutRenderer();
-            renderer.HttpContextAccessor = new FakeHttpContextAccessor(httpContext);
-            renderer.CookieNames = new List<string> { "key", "key2" };
+            var renderer = CreateRenderer(addCookie2: true);
             renderer.OutputFormat = AspNetRequestLayoutOutputFormat.Json;
 
             string result = renderer.Render(new LogEventInfo());
@@ -237,9 +187,46 @@ namespace NLog.Web.Tests.LayoutRenderers
             var t = (DebugTarget)LogManager.Configuration.AllTargets[0];
             var renderer = ((SimpleLayout)t.Layout).Renderers[0] as AspNetRequestCookieLayoutRenderer;
 
-            var result = renderer.Render(LogEventInfo.CreateNullEvent());           
+            var result = renderer.Render(LogEventInfo.CreateNullEvent());
 
             Assert.Equal(expectedResult, result);
+        }
+
+        /// <summary>
+        /// Create cookie renderer with mockup http context
+        /// </summary>
+        /// <param name="addKey">add key1 to cookie 1</param>
+        /// <param name="addCookie2">add 2nd cookie</param>
+        /// <returns></returns>
+        private static AspNetRequestCookieLayoutRenderer CreateRenderer(bool addKey = true, bool addCookie2 = false)
+        {
+            var httpContext = Substitute.For<HttpContextBase>();
+            var cookie1 = new HttpCookie("key", "TEST");
+            if (addKey)
+            {
+                cookie1["Key1"] = "TEST1";
+            }
+
+            var cookies = new HttpCookieCollection { cookie1 };
+            var cookieNames = new List<string> { "key" };
+            if (addCookie2)
+            {
+                var cookie2 = new HttpCookie("key2", "Test");
+                cookie2["key3"] = "Test456";
+                cookies.Add(cookie2);
+                cookieNames.Add("key2");
+            }
+
+            httpContext.Request.Cookies.Returns(cookies);
+            var renderer = new AspNetRequestCookieLayoutRenderer();
+            renderer.HttpContextAccessor = new FakeHttpContextAccessor(httpContext);
+
+            renderer.CookieNames = cookieNames;
+
+
+
+
+            return renderer;
         }
     }
 }
