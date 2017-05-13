b222270f62746f796c0924ad42dfd039958006ae
Julian Verdurmen <304NotModified@users.noreply.github.com>
2017-05-13 16:45:29 +0200

    Added ${aspnet-request-ip}

---
+++ NLog.Web.AspNetCore/LayoutRenderers/AspNetRequestIpLayoutRenderer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+#if NETSTANDARD_1plus
+using Microsoft.AspNetCore.Http;
+#endif
+using NLog.Config;
+using NLog.LayoutRenderers;
+using NLog.Web.Internal;
+
+namespace NLog.Web.LayoutRenderers
+{
+    /// <summary>
+    /// Render the request IP for ASP.NET Core
+    /// </summary>
+    /// <example>
+    /// <code lang="NLog Layout Renderer">
+    /// ${aspnet-request-ip}
+    /// </code>
+    /// </example>
+    [LayoutRenderer("aspnet-request-ip")]
+    public class AspNetRequestIpLayoutRenderer : AspNetLayoutRendererBase
+    {
+        /// <summary>
+        /// Render IP
+        /// </summary>
+        protected override void DoAppend(StringBuilder builder, LogEventInfo logEvent)
+        {
+            var httpContext = HttpContextAccessor.HttpContext;
+            if (httpContext == null)
+            {
+                return;
+            }
+#if !NETSTANDARD_1plus
+
+            var ip = httpContext.Request?.ServerVariables["REMOTE_ADDR"];
+#else
+            var ip = httpContext.Connection?.RemoteIpAddress;
+#endif
+            builder.Append(ip);
+        }
+    }
+}
