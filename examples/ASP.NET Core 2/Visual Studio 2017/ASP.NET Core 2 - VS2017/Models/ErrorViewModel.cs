6f861ea2faae58a16e43b5d3fb9540839833cbb4
Julian Verdurmen <304NotModified@users.noreply.github.com>
2017-10-07 23:09:38 +0200

    Added ASP.NET Core 2 example

---
+++ examples/ASP.NET Core 2/Visual Studio 2017/ASP.NET Core 2 - VS2017/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASP.NET_Core_2___VS2017.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
