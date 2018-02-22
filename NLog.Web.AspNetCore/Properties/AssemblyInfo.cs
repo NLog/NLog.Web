5bddeaae77e1d6e20b86aecd5cbb9dd892f09367
Rolf Kristensen <sweaty1@hotmail.com>
2018-02-22 16:10:12 +0100

    Removed System.ValueTuple as dependency for NetCoreApp1 (And fixed FileVersion)

--- NLog.Web.AspNetCore/Properties/AssemblyInfo.cs
+++ NLog.Web.AspNetCore/Properties/AssemblyInfo.cs
@@ -2,21 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("NLog.Web.AspNetCore")]
-[assembly: AssemblyProduct("NLog.Web for ASP.NET Core")]
-
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-
-[assembly: AssemblyCopyright("Copyright © NLog 2015-2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("4.0.0.0")] //fixed
-
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
