82f3ccc3e16eb5abe45f2f99c7ade042ecfc8525
Alex Angas <alexangas@gmail.com>
2018-06-22 14:31:43 +0100

    Tests for updated AssemblyVersion layout renderer logic

--- NLog.Web.AspNetCore.Tests/LayoutRenderers/AssemblyVersionLayoutRendererTests.cs
+++ NLog.Web.AspNetCore.Tests/LayoutRenderers/AssemblyVersionLayoutRendererTests.cs
@@ -13,9 +13,25 @@ namespace NLog.Web.AspNetCore.Tests.LayoutRenderers
         [Fact]
         public void AssemblyNameVersionTest()
         {
-            Layout l = "${assembly-version:NLog.Web.AspNetCore.Tests}";
-            var result = l.Render(LogEventInfo.CreateNullEvent());
+            Layout layout = "${assembly-version:NLog.Web.AspNetCore.Tests}";
+            var result = layout.Render(LogEventInfo.CreateNullEvent());
             Assert.Equal("1.2.3.0", result);
+        }
+        
+        [Fact]
+        public void AssemblyNameFileVersionTest()
+        {
+            Layout layout = "${assembly-version:name=NLog.Web.AspNetCore.Tests:type=file}";
+            var result = layout.Render(LogEventInfo.CreateNullEvent());
+            Assert.Equal("1.2.3.1", result);
+        }
+        
+        [Fact]
+        public void AssemblyNameInformationalVersionTest()
+        {
+            Layout layout = "${assembly-version:name=NLog.Web.AspNetCore.Tests:type=informational}";
+            var result = layout.Render(LogEventInfo.CreateNullEvent());
+            Assert.Equal("1.2.3.2", result);
         }
     }
 }
