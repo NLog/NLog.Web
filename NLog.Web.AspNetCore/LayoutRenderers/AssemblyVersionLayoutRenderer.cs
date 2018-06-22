dccb585a8da109342aef64fd8421f0fe40c3a3dd
Alex Angas <alexangas@gmail.com>
2018-06-22 14:16:22 +0100

    Copied AssemblyVersion layout renderer logic from NLog

--- NLog.Web.AspNetCore/LayoutRenderers/AssemblyVersionLayoutRenderer.cs
+++ NLog.Web.AspNetCore/LayoutRenderers/AssemblyVersionLayoutRenderer.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 using NLog.Common;
 using NLog.Config;
+using NLog.Web.Internal;
 using NLog.LayoutRenderers;
 
 #if ASP_NET_CORE
@@ -24,6 +26,14 @@ namespace NLog.Web.LayoutRenderers
     public class AssemblyVersionLayoutRenderer : LayoutRenderer
     {
         /// <summary>
+        /// Initializes a new instance of the <see cref="AssemblyVersionLayoutRenderer" /> class.
+        /// </summary>
+        public AssemblyVersionLayoutRenderer()
+        {
+            Type = AssemblyVersionType.Assembly;
+        }
+
+        /// <summary>
         /// The (full) name of the assembly. If <c>null</c>, using:
         /// 1) for .NET Standard - the runtime framework (for example, for .NET Core 1.1 this layout renderer returned value "1.1"),
         /// 2) for .NET Full - the entry assembly.
@@ -32,61 +42,83 @@ namespace NLog.Web.LayoutRenderers
         public string Name { get; set; }
 
         /// <summary>
-        /// Implemented by subclasses to render request information and append it to the specified <see cref="StringBuilder" />.
-        /// 
-        /// Won't be called if <see cref="AspNetLayoutRendererBase.HttpContextAccessor"/> of <see cref="IHttpContextAccessor.HttpContext"/> is <c>null</c>.
+        /// Gets or sets the type of assembly version to retrieve.
+        /// </summary>
+        [DefaultValue(nameof(AssemblyVersionType.Assembly))]
+        public AssemblyVersionType Type { get; set; }
+
+        /// <summary>
+        /// Initializes the layout renderer.
+        /// </summary>
+        protected override void InitializeLayoutRenderer()
+        {
+            _assemblyVersion = null;
+            base.InitializeLayoutRenderer();
+        }
+
+        /// <summary>
+        /// Closes the layout renderer.
+        /// </summary>
+        protected override void CloseLayoutRenderer()
+        {
+            _assemblyVersion = null;
+            base.CloseLayoutRenderer();
+        }
+
+        private string _assemblyVersion;
+
+        /// <summary>
+        /// Renders an assembly version and appends it to the specified <see cref="StringBuilder" />.
         /// </summary>
         /// <param name="builder">The <see cref="StringBuilder"/> to append the rendered data to.</param>
         /// <param name="logEvent">Logging event.</param>
         protected override void Append(StringBuilder builder, LogEventInfo logEvent)
         {
-            InternalLogger.Trace("Using ${assembly-version} of NLog.Web");
+            InternalLogger.Trace("Replacing AssemblyVersion Layout Renderer ${assembly-version} with NLog.Web implementation");
 
-            var nameNotEmpty = !string.IsNullOrEmpty(Name);
-            if (nameNotEmpty)
+            var version = _assemblyVersion ?? (_assemblyVersion = GetVersion());
+
+            if (string.IsNullOrEmpty(version))
             {
-                var assembly = Assembly.Load(new AssemblyName(Name));
+                version = $"Could not find value for {(string.IsNullOrEmpty(Name) ? "entry" : Name)} assembly and version type {Type}";
+            }
+
+            builder.Append(version);
+        }
+
+        private string GetVersion()
+        {
+            var assembly = GetAssembly();
+            return GetVersion(assembly);
+        }
+
+        private System.Reflection.Assembly GetAssembly()
+        {
+            if (string.IsNullOrEmpty(Name))
+            {
+                var assembly = System.Reflection.Assembly.GetEntryAssembly();
+#if !ASP_NET_CORE
                 if (assembly == null)
                 {
-                    builder.Append("Could not find assembly " + Name);
+                    assembly = GetAspNetEntryAssembly();
                 }
-                else
-                {
-                    builder.Append(assembly.GetName().Version.ToString());
-                }
+#endif
+                return assembly;
             }
             else
             {
-                string assemblyVersion = GetAssemblyVersion();
-                builder.Append(assemblyVersion ?? "Could not entry assembly");
+                return System.Reflection.Assembly.Load(new System.Reflection.AssemblyName(Name));
             }
         }
 
-
-        private static string GetAssemblyVersion()
-        {
-            var assembly = Assembly.GetEntryAssembly();
-
 #if !ASP_NET_CORE
-            if (assembly == null)
-            {
-                assembly = GetAspNetEntryAssembly();
-            }
-#endif
-           
-            var version = assembly?.GetName().Version.ToString();
-            return version;
-            
-        }
-#if !ASP_NET_CORE
-
         private static Assembly GetAspNetEntryAssembly()
         {
             if (System.Web.HttpContext.Current == null || System.Web.HttpContext.Current.ApplicationInstance == null)
             {
                 return null;
             }
-           var type = System.Web.HttpContext.Current.ApplicationInstance.GetType();
+            var type = System.Web.HttpContext.Current.ApplicationInstance.GetType();
             while (type != null && type.Namespace == "ASP")
             {
                 type = type.BaseType;
@@ -94,5 +126,20 @@ namespace NLog.Web.LayoutRenderers
             return type != null ? type.Assembly : null;
         }
 #endif
+
+        private string GetVersion(System.Reflection.Assembly assembly)
+        {
+            switch (Type)
+            {
+                case AssemblyVersionType.File:
+                    return ReflectionHelpers.GetCustomAttribute<System.Reflection.AssemblyFileVersionAttribute>(assembly)?.Version;
+
+                case AssemblyVersionType.Informational:
+                    return ReflectionHelpers.GetCustomAttribute<System.Reflection.AssemblyInformationalVersionAttribute>(assembly)?.InformationalVersion;
+
+                default:
+                    return assembly?.GetName().Version?.ToString();
+            }
+        }
     }
 }
---
+++ NLog.Web.AspNetCore/LayoutRenderers/AssemblyVersionType.cs
@@ -0,0 +1,23 @@
+﻿namespace NLog.Web.LayoutRenderers
+{
+    /// <summary>
+    /// Type of assembly version to retrieve.
+    /// </summary>
+    public enum AssemblyVersionType
+    {
+        /// <summary>
+        /// Gets the assembly version.
+        /// </summary>
+        Assembly,
+
+        /// <summary>
+        /// Gets the file version.
+        /// </summary>
+        File,
+
+        /// <summary>
+        /// Gets additional version information.
+        /// </summary>
+        Informational
+    }
+}
