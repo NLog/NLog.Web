294ad5d50029e44c9385ecbddf8b5dcdac7c1051
Rolf Kristensen <sweaty1@hotmail.com>
2018-12-02 19:33:41 +0100

    Added aspnet-traceidentifier for AspNet for NLog.Web

--- NLog.Web.AspNetCore/LayoutRenderers/AspNetTraceIdentifierLayoutRenderer.cs
+++ NLog.Web.AspNetCore/LayoutRenderers/AspNetTraceIdentifierLayoutRenderer.cs
@@ -17,7 +17,38 @@ namespace NLog.Web.LayoutRenderers
         protected override void DoAppend(StringBuilder builder, LogEventInfo logEvent)
         {
             var httpContext = HttpContextAccessor.HttpContext;
-            builder.Append(httpContext.TraceIdentifier);
+            builder.Append(LookupTraceIdentifier(httpContext));
         }
+
+#if ASP_NET_CORE
+        private string LookupTraceIdentifier(Microsoft.AspNetCore.Http.HttpContext httpContext)
+        {
+            return httpContext.TraceIdentifier;
+        }
+#else
+        /// <summary>
+        /// Requires IIS ETW feature enabled. https://docs.microsoft.com/en-us/iis/configuration/system.webServer/httpTracing/
+        ///
+        /// See also http://blog.tatham.oddie.com.au/2012/02/07/code-request-correlation-in-asp-net/
+        /// </summary>
+        private string LookupTraceIdentifier(System.Web.HttpContextBase httpContext)
+        {
+            IServiceProvider serviceProvider = httpContext;
+            if (serviceProvider != null)
+            {
+                var workerRequest = (System.Web.HttpWorkerRequest)serviceProvider.GetService(typeof(System.Web.HttpWorkerRequest));
+                if (workerRequest != null)
+                {
+                    Guid requestIdGuid = workerRequest.RequestTraceIdentifier;
+                    if (requestIdGuid != Guid.Empty)
+                    {
+                        return requestIdGuid.ToString();
+                    }
+                }
+            }
+
+            return null;
+        }
+#endif
     }
 }
