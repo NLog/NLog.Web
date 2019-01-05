ea5fd6b5286a38c7b5e6aeaeaf3d4aeba3592754
Rolf Kristensen <sweaty1@hotmail.com>
2019-01-05 11:21:06 +0100

    Enabled SymbolPackageFormat=snupkg

--- build_aspnetcore.bat
+++ build_aspnetcore.bat
@@ -3,5 +3,5 @@ echo off
 rem update NLog.Web.AspNetCore.csproj for version number
 
 cd NLog.Web.AspNetCore
-msbuild    /t:restore /t:build /p:configuration=release /verbosity:minimal
+msbuild /t:restore /t:build /p:configuration=release /verbosity:minimal /p:IncludeSymbols=true /p:SymbolPackageFormat=snupkg
 cd ..
