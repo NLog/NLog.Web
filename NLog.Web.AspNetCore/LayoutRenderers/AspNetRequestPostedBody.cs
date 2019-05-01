c724665f281050a2a27f76c10488a3b5fd77f8c4
Rolf Kristensen <sweaty1@hotmail.com>
2019-05-01 23:28:37 +0200

    AspNetRequestPostedBody - Added MaxContentLength protection

--- NLog.Web.AspNetCore/LayoutRenderers/AspNetRequestPostedBody.cs
+++ NLog.Web.AspNetCore/LayoutRenderers/AspNetRequestPostedBody.cs
@@ -8,6 +8,9 @@ using NLog.Web.Internal;
 
 #if !ASP_NET_CORE
 using System.Web;
+using HttpRequest = System.Web.HttpRequestBase;
+#else
+using HttpRequest = Microsoft.AspNetCore.Http.HttpRequest;
 #endif
 
 namespace NLog.Web.LayoutRenderers
@@ -26,6 +29,11 @@ namespace NLog.Web.LayoutRenderers
     public class AspNetRequestPostedBody : AspNetLayoutRendererBase
     {
         /// <summary>
+        /// Skip logging of HttpRequest.Body when ContentLength is larger than this (0 = No limit, -1 = No Buffer Limit)
+        /// </summary>
+        public int MaxContentLength { get; set; } = 30 * 1024;
+
+        /// <summary>
         /// Renders the ASP.NET posted body
         /// </summary>
         /// <param name="builder">The <see cref="StringBuilder" /> to append the rendered data to.</param>
@@ -38,9 +46,16 @@ namespace NLog.Web.LayoutRenderers
                 return;
             }
 
-            if (httpRequest.ContentLength <= 0)
+            long? contentLength = httpRequest.ContentLength;
+            if (contentLength <= 0)
             {
                 return; // No Body to read
+            }
+
+            if (MaxContentLength > 0 && contentLength > MaxContentLength)
+            {
+                InternalLogger.Debug("AspNetRequestPostedBody: body stream is too big. ContentLength={0}", contentLength);
+                return; // Body too large
             }
 
 #if !ASP_NET_CORE
@@ -61,26 +76,72 @@ namespace NLog.Web.LayoutRenderers
                 return;
             }
 
-            long oldPosition = -1;
-
             // reset if possible
-            if (body.CanSeek)
+            long oldPosition = body.Position;
+            if (!body.CanSeek)
             {
-                oldPosition = body.Position;
-                body.Position = 0;
+                if (oldPosition > 0 && oldPosition >= contentLength)
+                {
+                    InternalLogger.Debug("AspNetRequestPostedBody: body stream cannot seek and already read. StreamPosition={0}", oldPosition);
+                    return;
+                }
+
+                oldPosition = 0;
+                if (!TryEnableBuffering(httpRequest, contentLength, out body))
+                {
+                    return; // Body cannot be buffered
+                }
             }
+            else
+            {
+                if (MaxContentLength > 0 && !contentLength.HasValue && body.Length > MaxContentLength)
+                {
+                    InternalLogger.Debug("AspNetRequestPostedBody: body stream too big. Body.Length={0}", body.Length);
+                    return; // Body too large
+                }
+            }
+
+            body.Position = 0;
 
             //note: dispose of StreamReader isn't doing things besides closing the stream (which can be turn off, and then it's a NOOP)
             var bodyReader = new StreamReader(body);
             var content = bodyReader.ReadToEnd();
 
             //restore
-            if (body.CanSeek)
-            {
-                body.Position = oldPosition;
-            }
+            body.Position = oldPosition;
 
             builder.Append(content);
+        }
+
+        bool TryEnableBuffering(HttpRequest httpRequest, long? contentLength, out Stream bodyStream)
+        {
+            bodyStream = null;
+
+            if (MaxContentLength >= 0 && !contentLength.HasValue)
+            {
+                InternalLogger.Debug("AspNetRequestPostedBody: body stream cannot seek with unknown ContentLength");
+                return false;
+            }
+
+            int bufferThreshold = MaxContentLength <= 0 ? 64 * 1024 : MaxContentLength;
+            if (MaxContentLength == 0 && contentLength > bufferThreshold)
+            {
+                InternalLogger.Debug("AspNetRequestPostedBody: body stream cannot seek and stream is too big. ContentLength={0}", contentLength);
+                return false;
+            }
+
+#if ASP_NET_CORE2
+            Microsoft.AspNetCore.Http.HttpRequestRewindExtensions.EnableBuffering(httpRequest, bufferThreshold);
+            bodyStream = httpRequest.Body;
+            return bodyStream?.CanSeek == true;
+#elif ASP_NET_CORE1
+            Microsoft.AspNetCore.Http.Internal.BufferingHelper.EnableRewind(httpRequest, bufferThreshold);
+            bodyStream = httpRequest.Body;
+            return bodyStream?.CanSeek == true;
+#else
+            InternalLogger.Debug("AspNetRequestPostedBody: body stream cannot seek");
+            return false;
+#endif
         }
     }
 }
