a3f67ff16f68ae39617428972c1cdb3390615198
Julian Verdurmen <304NotModified@users.noreply.github.com>
2019-06-05 19:39:20 +0200

    Fix NotSupportedException with ${aspnet-request-posted-body} + updated example (#431)

---
+++ examples/ASP.NET Core 2/Visual Studio 2017/ASP.NET Core 2 - VS2017/Models/MessageModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NLog.Web.AspNetCore2.Example.Models
+{
+    public class MessageModel
+    {
+        public string From { get; set; }
+        public string Message { get; set; }
+    }
+}
