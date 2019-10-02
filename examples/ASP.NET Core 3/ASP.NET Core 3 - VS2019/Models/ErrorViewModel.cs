5e7ac4c20fd385e4b97bee638ee438a83de007f6
Julian Verdurmen <304NotModified@users.noreply.github.com>
2019-10-02 23:42:50 +0200

    Created ASP.NET Core 3 example site

---
+++ examples/ASP.NET Core 3/ASP.NET Core 3 - VS2019/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASP.NET_Core_3___VS2019.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
