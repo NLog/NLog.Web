bd557c97c1fc4829229c5a5f14976dc9b432ecd3
Rolf Kristensen <sweaty1@hotmail.com>
2020-03-31 01:30:30 +0200

    LogManager.Setup() - SetupBuilderExtension with LoadConfigurationFromAppSettings

---
+++ src/NLog.Web.AspNetCore/Config/SetupBuilderExtensions.cs
@@ -0,0 +1,103 @@
+﻿using System;
+#if !ASP_NET_CORE1 && !ASP_NET_CORE2
+using System.IO;
+using System.Linq;
+#endif
+using Microsoft.Extensions.Configuration;
+using NLog.Config;
+using NLog.Extensions.Logging;
+
+namespace NLog.Web
+{
+    /// <summary>
+    /// Extension methods to setup LogFactory options
+    /// </summary>
+    public static class SetupBuilderExtensions
+    {
+#if !ASP_NET_CORE1 && !ASP_NET_CORE2
+        /// <summary>
+        /// Loads NLog LoggingConfiguration from appsettings.json from the NLog-section
+        /// </summary>
+        public static ISetupBuilder LoadConfigurationFromAppSettings(this ISetupBuilder setupBuilder, string basePath = null, string environment = null, string nlogConfigSection = "NLog", bool optional = true, bool reloadOnChange = false)
+        {
+            environment = environment ?? GetAspNetCoreEnvironment("DOTNET_ENVIRONMENT") ?? GetAspNetCoreEnvironment("ASPNETCORE_ENVIRONMENT") ?? "Production";
+
+            var builder = new ConfigurationBuilder()
+                // Host Configuration
+                .SetBasePath(basePath ?? Directory.GetCurrentDirectory())
+                .AddEnvironmentVariables(prefix: "ASPNETCORE_")
+                .AddEnvironmentVariables(prefix: "DOTNET_")
+                // App Configuration
+                .AddJsonFile("appsettings.json", optional, reloadOnChange)
+                .AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: reloadOnChange)
+                .AddEnvironmentVariables();
+
+            var config = builder.Build();
+            if (!string.IsNullOrEmpty(nlogConfigSection) && config.GetSection(nlogConfigSection)?.GetChildren().Any() == true)
+            {
+                return setupBuilder.SetupExtensions(e => e.RegisterNLogWeb()).LoadConfigurationFromSection(config, nlogConfigSection);
+            }
+            else
+            {
+                setupBuilder.SetupExtensions(e => e.RegisterNLogWeb().RegisterConfigSettings(config));
+
+                if (!string.IsNullOrEmpty(basePath))
+                {
+                    if (!string.IsNullOrEmpty(environment))
+                    {
+                        setupBuilder.LoadConfigurationFromFile(Path.Combine(basePath, $"nlog.{environment}.config"), optional: true);
+                    }
+
+                    setupBuilder.LoadConfigurationFromFile(Path.Combine(basePath, "nlog.config"), optional: true);
+                }
+                else if (!string.IsNullOrEmpty(environment))
+                {
+                    setupBuilder.LoadConfigurationFromFile($"nlog.{environment}.config", optional: true);
+                }
+
+                return setupBuilder.LoadConfigurationFromFile();    // No effect, if config already loaded
+            }
+        }
+
+        private static string GetAspNetCoreEnvironment(string variableName)
+        {
+            try
+            {
+                var environment = Environment.GetEnvironmentVariable(variableName);
+                if (string.IsNullOrWhiteSpace(environment))
+                    return null;
+
+                return environment.Trim();
+            }
+            catch (Exception ex)
+            {
+                NLog.Common.InternalLogger.Error(ex, "Failed to lookup environment variable {0}", variableName);
+                return null;
+            }
+        }
+#endif
+
+        /// <summary>
+        /// Convience method to register aspnet-layoutrenders in NLog.Web as one-liner before loading NLog.config
+        /// </summary>
+        /// <remarks>
+        /// If not providing <paramref name="serviceProvider"/>, then output from aspnet-layoutrenderers will remain empty
+        /// </remarks>
+        public static ISetupBuilder RegisterNLogWeb(this ISetupBuilder setupBuilder, IConfiguration configuration = null, IServiceProvider serviceProvider = null)
+        {
+            setupBuilder.SetupExtensions(e => e.RegisterNLogWeb(serviceProvider));
+
+            if (configuration == null && serviceProvider != null)
+            {
+                configuration = serviceProvider.GetService(typeof(IConfiguration)) as IConfiguration;
+            }
+
+            if (configuration != null)
+            { 
+                setupBuilder.SetupExtensions(e => e.RegisterConfigSettings(configuration));
+            }
+
+            return setupBuilder;
+        }
+    }
+}
---
+++ src/NLog.Web.AspNetCore/Config/SetupExtensionsBuilderExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using NLog.Config;
+using NLog.Extensions.Logging;
+using NLog.Web.DependencyInjection;
+
+namespace NLog.Web
+{
+    /// <summary>
+    /// Extension methods to setup NLog extensions, so they are known when loading NLog LoggingConfiguration
+    /// </summary>
+    public static class SetupExtensionsBuilderExtensions
+    {
+        /// <summary>
+        /// Replace with version from NLog.Extension.Logging when it has been released with NLog 4.7
+        /// </summary>
+        internal static ISetupExtensionsBuilder RegisterConfigSettings(this ISetupExtensionsBuilder setupBuilder, IConfiguration configuration)
+        {
+            ConfigSettingLayoutRenderer.DefaultConfiguration = configuration;
+            return setupBuilder.RegisterLayoutRenderer<ConfigSettingLayoutRenderer>("configsetting");
+        }
+
+        /// <summary>
+        /// Register the NLog.Web.AspNetCore LayoutRenderers
+        /// </summary>
+        /// <remarks>
+        /// If not providing <paramref name="serviceProvider"/>, then output from aspnet-layoutrenderers will remain empty
+        /// </remarks>
+        public static ISetupExtensionsBuilder RegisterNLogWeb(this ISetupExtensionsBuilder setupBuilder, IServiceProvider serviceProvider = null)
+        {
+            if (serviceProvider != null)
+            {
+                ServiceLocator.ServiceProvider = serviceProvider;
+            }
+
+            return setupBuilder.RegisterAssembly(typeof(NLogAspNetCoreOptions).GetTypeInfo().Assembly);
+        }
+    }
+}
