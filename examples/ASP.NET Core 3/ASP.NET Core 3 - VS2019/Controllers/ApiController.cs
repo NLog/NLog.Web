8457ad4e88147ff2d9c4ed421b86241acb4b7aee
Julian Verdurmen <304NotModified@users.noreply.github.com>
2020-05-11 22:25:41 +0200

    Added api controller

---
+++ examples/ASP.NET Core 3/ASP.NET Core 3 - VS2019/Controllers/ApiController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ASP.NET_Core_3___VS2019.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogTestsController : ControllerBase
+    {
+        private readonly ILogger<LogTestsController> _logger;
+
+        public LogTestsController(ILogger<LogTestsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> TestLog([FromBody]string request)
+        {
+            try
+            {
+                throw new ApplicationException("Test log message");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Test error logging");
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+    }
+}
