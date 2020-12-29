360686e7b87235c508fd689f1a4aafd78f2c7644
Julian Verdurmen <304NotModified@users.noreply.github.com>
2020-12-29 16:31:44 +0100

    ASP.NET Core 5 example

---
+++ examples/ASP.NET Core 5/ASP.NET Core 5 NLog Example/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using ASP.NET_Core_5_NLog_Example.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Core_5_NLog_Example.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ examples/ASP.NET Core 5/ASP.NET Core 5 NLog Example/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASP.NET_Core_5_NLog_Example.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
