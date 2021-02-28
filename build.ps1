1ad7d8e6378a03a56abd165e7383c48d85867d4c
Julian Verdurmen <304NotModified@users.noreply.github.com>
2021-02-28 22:43:51 +0100

    Move to build.ps1

---
+++ build.ps1
@@ -0,0 +1,28 @@
+# restore and builds all projects as release.
+# creates NuGet package at \artifacts
+dotnet --version
+
+$versionPrefix = "4.11.0" # Also update version for minor versions in appveyor.yml 
+$versionSuffix = ""
+$versionFile = $versionPrefix + "." + ${env:APPVEYOR_BUILD_NUMBER}
+if ($env:APPVEYOR_PULL_REQUEST_NUMBER) {
+    $versionPrefix = $versionFile
+    $versionSuffix = "PR" + $env:APPVEYOR_PULL_REQUEST_NUMBER
+}
+
+msbuild NLog.Web.sln /t:restore,rebuild /p:configuration=release /p:ContinuousIntegrationBuild=true /verbosity:minimal
+if (-Not $LastExitCode -eq 0) {
+    exit $LastExitCode 
+}
+
+msbuild src\NLog.Web /t:rebuild,pack /p:configuration=release /verbosity:minimal /p:IncludeSymbols=true /p:SymbolPackageFormat=snupkg /p:ContinuousIntegrationBuild=true /p:VersionPrefix=$versionPrefix /p:VersionSuffix=$versionSuffix /p:FileVersion=$versionFile
+if (-Not $LastExitCode -eq 0) {
+    exit $LastExitCode 
+}
+
+msbuild src\NLog.Web.AspNetCore /t:rebuild,pack /p:configuration=release /verbosity:minimal /p:IncludeSymbols=true /p:SymbolPackageFormat=snupkg /p:ContinuousIntegrationBuild=true /p:VersionPrefix=$versionPrefix /p:VersionSuffix=$versionSuffix /p:FileVersion=$versionFile
+if (-Not $LastExitCode -eq 0) {
+    exit $LastExitCode 
+}
+
+exit $LastExitCode
