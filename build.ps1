a362139d442dda59f6930605dd1182f0982a728f
Julian Verdurmen <304NotModified@users.noreply.github.com>
2021-05-04 00:29:10 +0200

    version

--- build.ps1
+++ build.ps1
@@ -2,8 +2,8 @@
 # creates NuGet package at \artifacts
 dotnet --version
 
-$versionPrefix = "4.12.0" # Also update version for minor versions in appveyor.yml 
-$versionSuffix = ""
+$versionPrefix = "4.13.0" # Also update version for minor versions in appveyor.yml 
+$versionSuffix = "readme-preview"
 $versionFile = $versionPrefix + "." + ${env:APPVEYOR_BUILD_NUMBER}
 if ($env:APPVEYOR_PULL_REQUEST_NUMBER) {
     $versionPrefix = $versionFile
