0b1f2a84526c5d1351ae57c23ad848e585d2e6fb
Rolf Kristensen <snakefoot@users.noreply.github.com>
2022-06-06 11:40:51 +0200

    AspNetRequestCookieLayoutRenderer + AspNetResponseCookieLayoutRenderer with better code-reuse (#791)

---
+++ src/NLog.Web/Internal/HttpCookieCollectionValues.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Web;
+
+namespace NLog.Web.Internal
+{
+    internal static class HttpCookieCollectionValues
+    {
+        internal static IEnumerable<KeyValuePair<string, string>> GetCookieValues(HttpCookieCollection cookies, List<string> cookieNames, HashSet<string> excludeNames, bool expandMultiValue)
+        {
+            if (cookieNames?.Count > 0)
+            {
+                return GetCookieNameValues(cookies, cookieNames, expandMultiValue);
+            }
+            else
+            {
+                return GetCookieAllValues(cookies, excludeNames, expandMultiValue);
+            }
+        }
+
+        private static IEnumerable<KeyValuePair<string, string>> GetCookieNameValues(HttpCookieCollection cookies, List<string> cookieNames, bool expandMultiValue)
+        {
+            foreach (var cookieName in cookieNames)
+            {
+                var httpCookie = cookies[cookieName];
+                if (httpCookie != null)
+                {
+                    if (expandMultiValue)
+                    {
+                        var values = httpCookie.Values;
+                        if (values?.Count > 1)
+                        {
+                            foreach (var cookieValue in GetCookieMultiValues(httpCookie.Name, values))
+                                yield return new KeyValuePair<string, string>(cookieValue.Key, cookieValue.Value);
+                            continue;
+                        }
+                    }
+
+                    yield return new KeyValuePair<string, string>(httpCookie.Name, httpCookie.Value);
+                }
+            }
+        }
+
+        private static IEnumerable<KeyValuePair<string, string>> GetCookieAllValues(HttpCookieCollection cookies, HashSet<string> excludeNames, bool expandMultiValue)
+        {
+            bool checkForExclude = excludeNames?.Count > 0;
+
+            foreach (string cookieName in cookies.Keys)
+            {
+                if (checkForExclude && excludeNames.Contains(cookieName))
+                    continue;
+
+                var httpCookie = cookies[cookieName];
+                if (httpCookie != null)
+                {
+                    if (expandMultiValue)
+                    {
+                        var values = httpCookie.Values;
+                        if (values?.Count > 1)
+                        {
+                            foreach (var cookieValue in GetCookieMultiValues(httpCookie.Name, values))
+                                yield return new KeyValuePair<string, string>(cookieValue.Key, cookieValue.Value);
+                            continue;
+                        }
+                    }
+
+                    yield return new KeyValuePair<string, string>(httpCookie.Name, httpCookie.Value);
+                }
+            }
+        }
+
+        private static IEnumerable<KeyValuePair<string, string>> GetCookieMultiValues(string firstKey, NameValueCollection httpCookiValues)
+        {
+            // Split multi-valued cookie, as allowed for in the HttpCookie API for backwards compatibility with classic ASP
+            var isFirst = true;
+            foreach (var multiValueKey in httpCookiValues.AllKeys)
+            {
+                var cookieKey = multiValueKey;
+                if (isFirst)
+                {
+                    cookieKey = firstKey;
+                    isFirst = false;
+                }
+                yield return new KeyValuePair<string, string>(cookieKey, httpCookiValues[multiValueKey]);
+            }
+        }
+    }
+}
