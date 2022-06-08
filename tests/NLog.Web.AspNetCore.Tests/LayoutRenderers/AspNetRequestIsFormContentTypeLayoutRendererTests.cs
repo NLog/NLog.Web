3444edd1a95568530975fb69a64588bbcfcc3772
Burak Akgerman <burak.akgerman@huntington.com>
2022-06-08 12:17:25 -0400

    Added renders for is form content type and is https

---
+++ tests/NLog.Web.AspNetCore.Tests/LayoutRenderers/AspNetRequestIsFormContentTypeLayoutRendererTests.cs
@@ -0,0 +1,64 @@
+﻿using NLog.Web.LayoutRenderers;
+using NSubstitute;
+using NSubstitute.ReturnsExtensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace NLog.Web.Tests.LayoutRenderers
+{
+    public class AspNetRequestIsFormContentTypeLayoutRendererTests : LayoutRenderersTestBase<AspNetRequestIsFormContentTypeLayoutRenderer>
+    {
+        [Fact]
+        public void TrueTest()
+        {
+            // Arrange
+            var (renderer, httpContext) = CreateWithHttpContext();
+
+            httpContext.Request.HasFormContentType.Returns(true);
+            // Act
+            string result = renderer.Render(new LogEventInfo());
+            // Assert
+            Assert.Equal("1", result);
+        }
+
+        [Fact]
+        public void FalseTest()
+        {
+            // Arrange
+            var (renderer, httpContext) = CreateWithHttpContext();
+
+            httpContext.Request.HasFormContentType.Returns(false);
+            // Act
+            string result = renderer.Render(new LogEventInfo());
+            // Assert
+            Assert.Equal("0", result);
+        }
+
+        [Fact]
+        public void NullTest()
+        {
+            // Arrange
+            var (renderer, httpContext) = CreateWithHttpContext();
+
+            httpContext.Request.ReturnsNull();
+            // Act
+            string result = renderer.Render(new LogEventInfo());
+            // Assert
+            Assert.Equal("0", result);
+        }
+
+        protected override void NullRendersEmptyString()
+        {
+            // Arrange
+            var (renderer, _) = CreateWithHttpContext();
+
+            // Act
+            string result = renderer.Render(LogEventInfo.CreateNullEvent());
+
+            // Assert
+            Assert.Equal("0", result);
+        }
+    }
+}
