71c7a60d6b1671c8888bc551c6cbb474489d76cf
Burak Akgerman <burak.akgerman@huntington.com>
2022-06-13 12:09:44 -0400

    Unit tests for 2 layout renderers

---
+++ tests/Shared/LayoutRenderers/AspNetRequestContentLengthLayoutRendererTests.cs
@@ -0,0 +1,58 @@
+﻿using NLog.Web.LayoutRenderers;
+using NSubstitute;
+using NSubstitute.ReturnsExtensions;
+using Xunit;
+
+namespace NLog.Web.Tests.LayoutRenderers
+{
+    public class AspNetRequestContentLengthLayoutRendererTests : LayoutRenderersTestBase<AspNetRequestContentLength>
+    {
+        [Fact]
+        public void SuccessText()
+        {
+            // Arrange
+            var (renderer, httpContext) = CreateWithHttpContext();
+
+#if ASP_NET_CORE
+            httpContext.Request.ContentLength = 57;
+#else
+            httpContext.Request.ContentLength.Returns(57);
+#endif
+            // Act
+            string result = renderer.Render(LogEventInfo.CreateNullEvent());
+
+            // Assert
+            Assert.Equal("57", result);
+        }
+
+        [Fact]
+        public void EmptyTest()
+        {
+            // Arrange
+            var (renderer, httpContext) = CreateWithHttpContext();
+
+#if ASP_NET_CORE
+            httpContext.Request.ContentLength = 0;
+#else
+            httpContext.Request.ContentLength.Returns(0);
+#endif
+            // Act
+            string result = renderer.Render(new LogEventInfo());
+            // Assert
+            Assert.Equal("", result);
+        }
+
+        [Fact]
+        public void NullTest()
+        {
+            // Arrange
+            var (renderer, httpContext) = CreateWithHttpContext();
+
+            httpContext.Request.ReturnsNull();
+            // Act
+            string result = renderer.Render(new LogEventInfo());
+            // Assert
+            Assert.Equal("", result);
+        }
+    }
+}
---
+++ tests/Shared/LayoutRenderers/AspNetRequestContentTypeLayoutRendererTests.cs
@@ -0,0 +1,49 @@
+﻿using NLog.Web.LayoutRenderers;
+using Xunit;
+
+namespace NLog.Web.Tests.LayoutRenderers
+{
+    public  class AspNetRequestContentTypeLayoutRendererTests : LayoutRenderersTestBase<AspNetRequestContentTypeLayoutRenderer>
+    {
+        [Fact]
+        public void SuccessText()
+        {
+            // Arrange
+            var (renderer, httpContext) = CreateWithHttpContext();
+
+            httpContext.Request.ContentType = "text/json";
+
+            // Act
+            string result = renderer.Render(LogEventInfo.CreateNullEvent());
+
+            // Assert
+            Assert.Equal("text/json", result);
+        }
+
+        [Fact]
+        public void EmptyTest()
+        {
+            // Arrange
+            var (renderer, httpContext) = CreateWithHttpContext();
+
+            httpContext.Request.ContentType = "";
+            // Act
+            string result = renderer.Render(new LogEventInfo());
+            // Assert
+            Assert.Equal("", result);
+        }
+
+        [Fact]
+        public void NullTest()
+        {
+            // Arrange
+            var (renderer, httpContext) = CreateWithHttpContext();
+
+            httpContext.Request.ContentType = null;
+            // Act
+            string result = renderer.Render(new LogEventInfo());
+            // Assert
+            Assert.Equal("", result);
+        }
+    }
+}
