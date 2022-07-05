a0fd592f2d6d846ceaf796bd54ea88b1764c2901
Burak Akgerman <burak.akgerman@huntington.com>
2022-07-05 11:07:40 -0400

    Fix review comment

--- src/Shared/LayoutRenderers/AspNetRequestServerVariableLayoutRenderer.cs
+++ src/Shared/LayoutRenderers/AspNetRequestServerVariableLayoutRenderer.cs
@@ -34,12 +34,12 @@ namespace NLog.Web.LayoutRenderers
         [DefaultParameter]
         public string Item { get; set; }
 
-/// <summary>
-/// Renders the specified ASP.NET Request variable and appends it to the specified <see cref="StringBuilder" />.
-/// </summary>
-/// <param name="builder">The <see cref="StringBuilder" /> to append the rendered data to.</param>
-/// <param name="logEvent">Logging event.</param>
-protected override void DoAppend(StringBuilder builder, LogEventInfo logEvent)
+        /// <summary>
+        /// Renders the specified ASP.NET Request variable and appends it to the specified <see cref="StringBuilder" />.
+        /// </summary>
+        /// <param name="builder">The <see cref="StringBuilder" /> to append the rendered data to.</param>
+        /// <param name="logEvent">Logging event.</param>
+        protected override void DoAppend(StringBuilder builder, LogEventInfo logEvent)
         {
             var httpRequest = HttpContextAccessor.HttpContext.TryGetRequest();
             if (httpRequest == null)
