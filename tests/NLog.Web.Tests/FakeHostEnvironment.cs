99f28ae5c0f85acf195832cfe745981b3980153e
Burak Akgerman <burak.akgerman@huntington.com>
2022-07-21 08:16:37 -0400

    Review fixes

--- tests/NLog.Web.Tests/FakeHostEnvironment.cs
+++ tests/NLog.Web.Tests/FakeHostEnvironment.cs
@@ -1,16 +1,4 @@
-﻿#if ASP_NET_CORE
-#if ASP_NET_CORE2
-using IHostEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
-using Microsoft.Extensions.FileProviders;
-#endif
-#if ASP_NET_CORE3
-using Microsoft.Extensions.Hosting;
-#endif
-using Microsoft.Extensions.FileProviders;
-using NLog.Web.DependencyInjection;
-#else
-using NLog.Web.Internal;
-#endif
+﻿using NLog.Web.Internal;
 
 namespace NLog.Web.Tests
 {
@@ -19,55 +7,6 @@ namespace NLog.Web.Tests
     /// </summary>
     public class FakeHostEnvironment : IHostEnvironment
     {
-#if ASP_NET_CORE
-#if ASP_NET_CORE2
-        /// <summary>
-        /// Gets or sets the name of the environment. The host automatically sets this property to the value
-        /// of the "ASPNETCORE_ENVIRONMENT" environment variable, or "environment" as specified in any other configuration source.
-        /// </summary>
-        public string EnvironmentName { get; set; }
-        /// <summary>
-        /// Gets or sets the name of the application. This property is automatically set by the host to the assembly containing
-        /// the application entry point.
-        /// </summary>
-        public string ApplicationName { get; set; }
-        /// <summary>
-        /// Gets or sets the absolute path to the directory that contains the web-servable application content files.
-        /// </summary>
-        public string WebRootPath { get; set; }
-        /// <summary>
-        /// Gets or sets an <see cref="T:Microsoft.Extensions.FileProviders.IFileProvider" /> pointing at <see cref="P:Microsoft.AspNetCore.Hosting.IHostingEnvironment.WebRootPath" />.
-        /// </summary>
-        public IFileProvider WebRootFileProvider { get; set; }
-        /// <summary>
-        /// Gets or sets the absolute path to the directory that contains the application content files.
-        /// </summary>
-        public string ContentRootPath { get; set; }
-        /// <summary>
-        /// Gets or sets an <see cref="T:Microsoft.Extensions.FileProviders.IFileProvider" /> pointing at <see cref="P:Microsoft.AspNetCore.Hosting.IHostingEnvironment.ContentRootPath" />.
-        /// </summary>
-        public IFileProvider ContentRootFileProvider { get; set; }
-#else
-        /// <summary>
-        /// Gets or sets the name of the application. This property is automatically set by the host to the assembly containing
-        /// the application entry point.
-        /// </summary>
-        public string ApplicationName { get; set; }
-        /// <summary>
-        /// Gets or sets an <see cref="T:Microsoft.Extensions.FileProviders.IFileProvider" /> pointing at <see cref="P:Microsoft.Extensions.Hosting.IHostEnvironment.ContentRootPath" />.
-        /// </summary>
-        public IFileProvider ContentRootFileProvider { get; set; }
-        /// <summary>
-        /// Gets or sets the absolute path to the directory that contains the application content files.
-        /// </summary>
-        public string ContentRootPath { get; set; }
-        /// <summary>
-        /// Gets or sets the name of the environment. The host automatically sets this property to the value of the
-        /// "environment" key as specified in configuration.
-        /// </summary>
-        public string EnvironmentName { get; set; }
-#endif
-#else
         /// <summary>Gets the name of the site.</summary>
         /// <returns>The name of the site.</returns>
         public string SiteName { get; set; }
@@ -83,6 +22,5 @@ namespace NLog.Web.Tests
         {
             return MappedPath;
         }
-#endif
     }
 }
