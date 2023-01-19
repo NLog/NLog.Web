3e55f44e44c3f871d3ff607b300c59840305ace3
Burak Akgerman <burak.akgerman@gmail.com>
2023-01-19 14:33:04 -0500

    AspNet Response HttpStatusCode LayoutRenderer as Enum.ToString (#921)

--- src/Shared/LayoutRenderers/AspNetResponseStatusCodeRenderer.cs
+++ src/Shared/LayoutRenderers/AspNetResponseStatusCodeRenderer.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System;
+using System.Net;
+using System.Text;
 using NLog.Config;
 using NLog.LayoutRenderers;
 using NLog.Web.Internal;
@@ -9,12 +11,31 @@ namespace NLog.Web.LayoutRenderers
     /// ASP.NET Response Status Code.
     /// </summary>
     /// <remarks>
-    /// <code>${aspnet-response-statuscode}</code>
+    /// <code>${aspnet-response-statuscode}          emits the http status code as integer</code>
+    /// <code>${aspnet-response-statuscode:Format=D} emits the http status code as integer</code>
+    /// <code>${aspnet-response-statuscode:Format=F} emits the http status code as enum-string-value</code>
+    /// <code>${aspnet-response-statuscode:Format=G} emits the http status code as enum-string-value</code>
+    /// <code>${aspnet-response-statuscode:Format=X} emits the http status code as hexadecimal</code>
     /// </remarks>
     /// <seealso href="https://github.com/NLog/NLog/wiki/AspNetResponse-StatusCode-Layout-Renderer">Documentation on NLog Wiki</seealso>
     [LayoutRenderer("aspnet-response-statuscode")]
     public class AspNetResponseStatusCodeRenderer : AspNetLayoutRendererBase
     {
+        /// <summary>
+        /// A valid enumeration format string, defaults to integer format
+        /// </summary>
+        /// <remarks>
+        /// Supported Values, Case Insensitive
+        /// D: outputs the HttpStatusCode enum as a integer
+        /// F: outputs the HttpStatusCode enum as a string if possible, otherwise an integer
+        /// G: outputs the HttpStatusCode enum as a string if possible, otherwise an integer
+        /// X: outputs the HttpStatusCode enum as a hexadecimal
+        /// </remarks>
+        /// <seealso href="https://learn.microsoft.com/en-us/dotnet/standard/base-types/enumeration-format-strings">Documentation on Enum Format Strings</seealso>
+
+        [DefaultParameter]
+        public string Format { get; set; } = "d";
+
         /// <inheritdoc/>
         protected override void DoAppend(StringBuilder builder, LogEventInfo logEvent)
         {
@@ -24,11 +45,22 @@ namespace NLog.Web.LayoutRenderers
                 return;
             }
 
-            int statusCode = httpResponse.StatusCode;
-
-            if (statusCode >= 100 && statusCode <= 599)
+            var statusCode = httpResponse.StatusCode;
+            if (statusCode < 100 || statusCode > 599)
             {
-                builder.Append(statusCode);
+                // Only output valid HTTP status codes
+                return;
+            }
+
+            try
+            {
+                builder.Append(((HttpStatusCode)statusCode).ToString(Format));
+            }
+            catch (Exception ex)
+            {
+                NLog.Common.InternalLogger.Error(ex, 
+                    "Error occurred outputting HttpStatusCode enum ToString() with format specifier of {0} and value of {1}",
+                    Format, statusCode);
             }
         }
     }
--- tests/Shared/LayoutRenderers/AspNetResponseStatusCodeRendererTests.cs
+++ tests/Shared/LayoutRenderers/AspNetResponseStatusCodeRendererTests.cs
@@ -1,4 +1,5 @@
-﻿using NLog.Web.LayoutRenderers;
+﻿using System.Net;
+using NLog.Web.LayoutRenderers;
 using NSubstitute;
 using Xunit;
 
@@ -7,10 +8,106 @@ namespace NLog.Web.Tests.LayoutRenderers
     public class AspNetResponseStatusCodeRendererTests : LayoutRenderersTestBase<AspNetResponseStatusCodeRenderer>
     {
         [Fact]
-        public void StatusCode_Set_Renderer()
+        public void StatusCode_Set_Renderer_DefaultFormat()
         {
             // Arrange
             var (renderer, httpContext) = CreateWithHttpContext();
+            httpContext.Response.StatusCode.Returns(200);
+
+            // Act
+            string result = renderer.Render(new LogEventInfo());
+
+            // Assert
+            Assert.Equal("200", result);
+        }
+
+        [Fact]
+        public void StatusCode_Set_Renderer_EnumFormatF()
+        {
+            // Arrange
+            var (renderer, httpContext) = CreateWithHttpContext();
+            renderer.Format = "f";
+
+            httpContext.Response.StatusCode.Returns(200);
+
+            // Act
+            string result = renderer.Render(new LogEventInfo());
+
+            // Assert
+            Assert.Equal(((HttpStatusCode)200).ToString(), result);
+        }
+
+        [Fact]
+        public void StatusCode_Set_Renderer_EnumFormatG()
+        {
+            // Arrange
+            var (renderer, httpContext) = CreateWithHttpContext();
+            renderer.Format = "g";
+
+            httpContext.Response.StatusCode.Returns(200);
+
+            // Act
+            string result = renderer.Render(new LogEventInfo());
+
+            // Assert
+            Assert.Equal(((HttpStatusCode)200).ToString(), result);
+        }
+
+        [Fact]
+        public void StatusCode_Set_Renderer_IntegerFormat()
+        {
+            // Arrange
+            var (renderer, httpContext) = CreateWithHttpContext();
+            renderer.Format = "d";
+
+            httpContext.Response.StatusCode.Returns(200);
+
+            // Act
+            string result = renderer.Render(new LogEventInfo());
+
+            // Assert
+            Assert.Equal("200", result);
+        }
+
+        [Fact]
+        public void StatusCode_Set_Renderer_HexFormat()
+        {
+            // Arrange
+            var (renderer, httpContext) = CreateWithHttpContext();
+            renderer.Format = "x";
+
+            httpContext.Response.StatusCode.Returns(200);
+
+            // Act
+            string result = renderer.Render(new LogEventInfo());
+
+            // Assert
+            Assert.Equal(((HttpStatusCode)200).ToString("x"), result);
+        }
+
+        [Fact]
+        public void StatusCode_Set_Renderer_InvalidFormat()
+        {
+            // Arrange
+            var (renderer, httpContext) = CreateWithHttpContext();
+            renderer.Format = "invalid";
+
+            httpContext.Response.StatusCode.Returns(200);
+
+            // Act
+            string result = renderer.Render(new LogEventInfo());
+
+            // Assert
+            Assert.Equal("", result);
+        }
+
+        [Fact]
+        public void StatusCode_Set_Renderer_UpperCaseIntegerFormat()
+        {
+            // Arrange
+            var (renderer, httpContext) = CreateWithHttpContext();
+            renderer.Format = "D";
+
             httpContext.Response.StatusCode.Returns(200);
 
             // Act
@@ -26,7 +123,7 @@ namespace NLog.Web.Tests.LayoutRenderers
         [InlineData(100, true)]
         [InlineData(599, true)]
         [InlineData(600, false)]
-        public void Only_Render_Valid_StatusCodes(int statusCode, bool shouldBeRendered)
+        public void Only_Render_Valid_StatusCodes_DefaultFormat(int statusCode, bool shouldBeRendered)
         {
             // Arrange
             var (renderer, httpContext) = CreateWithHttpContext();
@@ -39,6 +136,142 @@ namespace NLog.Web.Tests.LayoutRenderers
             if (shouldBeRendered)
             {
                 Assert.Equal($"{statusCode}", result);
+            }
+            else
+            {
+                Assert.Empty(result);
+            }
+        }
+
+        [Theory]
+        [InlineData(0, false)]
+        [InlineData(99, false)]
+        [InlineData(100, true)]
+        [InlineData(599, true)]
+        [InlineData(600, false)]
+        public void Only_Render_Valid_StatusCodes_EnumFormatF(int statusCode, bool shouldBeRendered)
+        {
+            // Arrange
+            var (renderer, httpContext) = CreateWithHttpContext();
+            renderer.Format = "f";
+            httpContext.Response.StatusCode.Returns(statusCode);
+
+            // Act
+            string result = renderer.Render(new LogEventInfo());
+
+            // Assert
+            if (shouldBeRendered)
+            {
+                Assert.Equal(((HttpStatusCode)statusCode).ToString(), result);
+            }
+            else
+            {
+                Assert.Empty(result);
+            }
+        }
+
+
+        [Theory]
+        [InlineData(0, false)]
+        [InlineData(99, false)]
+        [InlineData(100, true)]
+        [InlineData(599, true)]
+        [InlineData(600, false)]
+        public void Only_Render_Valid_StatusCodes_EnumFormatG(int statusCode, bool shouldBeRendered)
+        {
+            // Arrange
+            var (renderer, httpContext) = CreateWithHttpContext();
+            renderer.Format = "g";
+            httpContext.Response.StatusCode.Returns(statusCode);
+
+            // Act
+            string result = renderer.Render(new LogEventInfo());
+
+            // Assert
+            if (shouldBeRendered)
+            {
+                Assert.Equal(((HttpStatusCode)statusCode).ToString(), result);
+            }
+            else
+            {
+                Assert.Empty(result);
+            }
+        }
+
+        [Theory]
+        [InlineData(0, false)]
+        [InlineData(99, false)]
+        [InlineData(100, true)]
+        [InlineData(599, true)]
+        [InlineData(600, false)]
+        public void Only_Render_Valid_StatusCodes_IntegerFormat(int statusCode, bool shouldBeRendered)
+        {
+            // Arrange
+            var (renderer, httpContext) = CreateWithHttpContext();
+            renderer.Format = "d";
+            httpContext.Response.StatusCode.Returns(statusCode);
+
+            // Act
+            string result = renderer.Render(new LogEventInfo());
+
+            // Assert
+            if (shouldBeRendered)
+            {
+                Assert.Equal($"{statusCode}", result);
+            }
+            else
+            {
+                Assert.Empty(result);
+            }
+        }
+
+        [Theory]
+        [InlineData(0, false)]
+        [InlineData(99, false)]
+        [InlineData(100, true)]
+        [InlineData(599, true)]
+        [InlineData(600, false)]
+        public void Only_Render_Valid_StatusCodes_UpperCaseIntegerFormat(int statusCode, bool shouldBeRendered)
+        {
+            // Arrange
+            var (renderer, httpContext) = CreateWithHttpContext();
+            renderer.Format = "D";
+            httpContext.Response.StatusCode.Returns(statusCode);
+
+            // Act
+            string result = renderer.Render(new LogEventInfo());
+
+            // Assert
+            if (shouldBeRendered)
+            {
+                Assert.Equal($"{statusCode}", result);
+            }
+            else
+            {
+                Assert.Empty(result);
+            }
+        }
+
+        [Theory]
+        [InlineData(0, false)]
+        [InlineData(99, false)]
+        [InlineData(100, true)]
+        [InlineData(599, true)]
+        [InlineData(600, false)]
+        public void Only_Render_Valid_StatusCodes_HexFormat(int statusCode, bool shouldBeRendered)
+        {
+            // Arrange
+            var (renderer, httpContext) = CreateWithHttpContext();
+            renderer.Format = "x";
+            httpContext.Response.StatusCode.Returns(statusCode);
+
+            // Act
+            string result = renderer.Render(new LogEventInfo());
+
+            // Assert
+            if (shouldBeRendered)
+            {
+                Assert.Equal(((HttpStatusCode)statusCode).ToString("x"), result);
             }
             else
             {
