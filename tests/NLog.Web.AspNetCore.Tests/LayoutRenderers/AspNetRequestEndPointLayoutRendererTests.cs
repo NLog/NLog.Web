4592c7eb456137f75ff0a293b6ad2d9eec8bec33
Rolf Kristensen <snakefoot@users.noreply.github.com>
2023-05-19 14:32:09 +0200

    aspnet-request-endpoint - EndPoint Routing DisplayName (#936)

---
+++ tests/NLog.Web.AspNetCore.Tests/LayoutRenderers/AspNetRequestEndPointLayoutRendererTests.cs
@@ -0,0 +1,43 @@
+﻿#if NETCOREAPP3_0_OR_GREATER
+using NLog.Web.LayoutRenderers;
+using Microsoft.AspNetCore.Connections.Features;
+using Microsoft.AspNetCore.Http.Features;
+using NSubstitute;
+using Xunit;
+
+namespace NLog.Web.Tests.LayoutRenderers
+{
+    public class AspNetRequestEndPointLayoutRendererTests : LayoutRenderersTestBase<AspNetRequestEndPointLayoutRenderer>
+    {
+        [Fact]
+        public void SuccessTest()
+        {
+            // Arrange
+            var (renderer, httpContext) = CreateWithHttpContext();
+            var endPointFeature = Substitute.For<IEndpointFeature>();
+            endPointFeature.Endpoint.Returns(new Microsoft.AspNetCore.Http.Endpoint(null, null, "42"));
+
+            var featureCollection = new FeatureCollection();
+            featureCollection.Set<IEndpointFeature>(endPointFeature);
+
+            httpContext.Features.Returns(featureCollection);
+            // Act
+            var result = renderer.Render(new LogEventInfo());
+            // Assert
+            Assert.Equal("42", result);
+        }
+
+        [Fact]
+        public void NullTest()
+        {
+            // Arrange
+            var (renderer, httpContext) = CreateWithHttpContext();
+            httpContext.Features.Returns(new FeatureCollection());
+            // Act
+            var result = renderer.Render(new LogEventInfo());
+            // Assert
+            Assert.Equal(string.Empty, result);
+        }
+    }
+}
+#endif
