40b1ba3f685eb50189381e2f108d547dfcb3f498
Rolf Kristensen <sweaty1@hotmail.com>
2023-05-19 16:11:35 +0200

    Added sample for ASP.NET Core 7.0 with minimal API

---
+++ examples/ASP.NET.Core7/Program.cs
@@ -0,0 +1,56 @@
+using NLog;
+using NLog.Web;
+
+var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger(); 
+
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Add NLog for Logging
+    builder.Logging.ClearProviders();
+    builder.Host.UseNLog();
+
+    // Add services to the container.
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+
+    var summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    app.MapGet("/weatherforecast", (ILogger<WeatherForecast> logger) =>
+    {
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+            new WeatherForecast
+            (
+                DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                Random.Shared.Next(-20, 55),
+                summaries[Random.Shared.Next(summaries.Length)]
+            ))
+            .ToArray();
+        logger.LogInformation("Generated {WeatherForecast}", forecast);
+        return forecast;
+    });
+
+    app.Run();
+}
+catch (Exception exception)
+{
+    // NLog: catch setup errors
+    logger.Error(exception, "Stopped program because of exception");
+    throw;
+}
+finally
+{
+    // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+    NLog.LogManager.Shutdown();
+}
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
