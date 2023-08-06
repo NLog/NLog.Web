9b4108c6d278947133dad814cbfa44141ac183c8
Burak Akgerman <burak.akgerman@huntington.com>
2023-08-06 14:05:14 -0400

    Verbose rendering for .NET Claim class

--- src/Shared/LayoutRenderers/AspNetUserClaimLayoutRenderer.cs
+++ src/Shared/LayoutRenderers/AspNetUserClaimLayoutRenderer.cs
@@ -9,6 +9,8 @@ using System.Text;
 using NLog.Common;
 using NLog.Config;
 using NLog.LayoutRenderers;
+using NLog.Layouts;
+using NLog.Web.Enums;
 
 namespace NLog.Web.LayoutRenderers
 {
@@ -28,10 +30,15 @@ namespace NLog.Web.LayoutRenderers
         /// </summary>
         /// <remarks>
         /// When value is prefixed with "ClaimTypes." (Remember dot) then ít will lookup in well-known claim types from <see cref="ClaimTypes"/>. Ex. ClaimsTypes.Name
-        /// If this is null or empty then all claim types are rendered
+        /// If this is null or empty then the Type and Value properties of all claim types are rendered
         /// </remarks>
         [DefaultParameter]
         public string ClaimType { get; set; }
+
+        /// <summary>
+        /// If this is true, then all string properties of the <see cref="Claim"/> are rendered as well the values in its Properties property.
+        /// </summary>
+        public bool Verbose { get; set; }
 
         /// <inheritdoc />
         protected override void InitializeLayoutRenderer()
@@ -64,15 +71,34 @@ namespace NLog.Web.LayoutRenderers
 
                 if (string.IsNullOrEmpty(ClaimType))
                 {
-                    var allClaims = GetAllClaims(claimsPrincipal);
-                    SerializePairs(allClaims, builder, logEvent);
+                    if (Verbose)
+                    {
+#if NET46
+                        SerializeVerbose((claimsPrincipal as ClaimsPrincipal)?.Claims, builder, logEvent);
+#else
+                        SerializeVerbose(claimsPrincipal.Claims, builder, logEvent);
+#endif
+                    }
+                    else
+                    {
+                        var allClaims = GetAllClaims(claimsPrincipal);
+                        SerializePairs(allClaims, builder, logEvent);
+                    }
                 }
                 else
                 {
                     var claim = GetClaim(claimsPrincipal, ClaimType);
                     if (claim != null)
                     {
-                        builder.Append(claim?.Value);
+                        if (Verbose)
+                        {
+                            SerializeVerbose(new List<Claim> {claim}, builder, logEvent);
+                        }
+                        else
+                        {
+                            builder.Append(claim.Value);
+
+                        }
                     }
                 }
             }
@@ -80,6 +106,174 @@ namespace NLog.Web.LayoutRenderers
             {
                 InternalLogger.Debug(ex, "aspnet-user-claim - HttpContext has been disposed");
             }
+        }
+
+        private void SerializeVerbose(IEnumerable<Claim> claims, StringBuilder builder, LogEventInfo logEvent)
+        {
+            if (claims == null)
+            {
+                return;
+            }
+
+            switch (OutputFormat)
+            {
+                case AspNetRequestLayoutOutputFormat.Flat:
+                    SerializeVerboseFlat(claims, builder, logEvent);
+                    break;
+                case AspNetRequestLayoutOutputFormat.JsonArray:
+                case AspNetRequestLayoutOutputFormat.JsonDictionary:
+                    SerializeVerboseJson(claims, builder, logEvent);
+                    break;
+            }
+        }
+
+        private void SerializeVerboseJson(IEnumerable<Claim> claims, StringBuilder builder, LogEventInfo logEvent)
+        {
+            var firstItem = true;
+            var includeSeparator = false;
+
+            foreach (var claim in claims)
+            {
+                if (firstItem)
+                {
+                    if (OutputFormat == AspNetRequestLayoutOutputFormat.JsonDictionary)
+                    {
+                        builder.Append('{');
+                    }
+                    else
+                    {
+                        builder.Append('[');
+                    }
+                }
+                else
+                {
+                    builder.Append(',');
+                }
+
+                builder.Append('{');
+
+                includeSeparator |= AppendJsonProperty(builder, nameof(claim.Type), claim.Type, false);
+                includeSeparator |= AppendJsonProperty(builder, nameof(claim.Value), claim.Value, includeSeparator);
+                includeSeparator |= AppendJsonProperty(builder, nameof(claim.ValueType), claim.ValueType, includeSeparator);
+
+                includeSeparator |= AppendJsonProperty(builder, nameof(claim.Issuer), claim.Issuer, includeSeparator);
+                includeSeparator |= AppendJsonProperty(builder, nameof(claim.OriginalIssuer), claim.OriginalIssuer, includeSeparator);
+
+                if (claim.Properties != null && claim.Properties.Count > 0)
+                {
+                    builder.Append(",\"");
+                    builder.Append(nameof(claim.Properties));
+                    builder.Append("\":");
+                    SerializePairs(claim.Properties.OrderBy(entry => entry.Key).ToList(), builder, logEvent);
+                }
+
+                builder.Append('}');
+
+                firstItem = false;
+            }
+
+            if (!firstItem)
+            {
+                if (OutputFormat == AspNetRequestLayoutOutputFormat.JsonDictionary)
+                {
+                    builder.Append('}');
+                }
+                else
+                {
+                    builder.Append(']');
+                }
+            }
+        }
+
+        private void SerializeVerboseFlat(IEnumerable<Claim> claims, StringBuilder builder, LogEventInfo logEvent)
+        {
+            var propertySeparator = GetRenderedItemSeparator(logEvent);
+            var valueSeparator = GetRenderedValueSeparator(logEvent);
+            var objectSeparator = GetRenderedObjectSeparator(logEvent);
+
+            var firstObject = true;
+            var includeSeparator = false;
+
+            foreach (var claim in claims)
+            {
+                if (!firstObject)
+                {
+                    builder.Append(objectSeparator);
+                }
+
+                firstObject = false;
+
+                includeSeparator |= AppendFlatProperty(builder, nameof(claim.Type), claim.Type, valueSeparator, "");
+                includeSeparator |= AppendFlatProperty(builder, nameof(claim.Value), claim.Value, valueSeparator, includeSeparator ? propertySeparator : "");
+                includeSeparator |= AppendFlatProperty(builder, nameof(claim.ValueType), claim.ValueType, valueSeparator, includeSeparator ? propertySeparator : "");
+
+                includeSeparator |= AppendFlatProperty(builder, nameof(claim.Issuer), claim.Issuer, valueSeparator, includeSeparator ? propertySeparator : "");
+                includeSeparator |= AppendFlatProperty(builder, nameof(claim.OriginalIssuer), claim.OriginalIssuer, valueSeparator, includeSeparator ? propertySeparator : "");
+
+                if (claim.Properties != null && claim.Properties.Count > 0)
+                {
+                    builder.Append(propertySeparator);
+                    builder.Append("Properties[");
+                    SerializePairs(claim.Properties.OrderBy(entry => entry.Key).ToList(), builder, logEvent);
+                    builder.Append(']');
+                }
+            }
+        }
+
+        /// <summary>
+        /// Separator between objects, like cookies. Only used for <see cref="AspNetRequestLayoutOutputFormat.Flat" />
+        /// </summary>
+        /// <remarks>Render with <see cref="GetRenderedObjectSeparator" /></remarks>
+        public string ObjectSeparator { get => _objectSeparatorLayout?.OriginalText; set => _objectSeparatorLayout = new SimpleLayout(value ?? ""); }
+        private SimpleLayout _objectSeparatorLayout = new SimpleLayout(";");
+
+        /// <summary>
+        /// Get the rendered <see cref="ObjectSeparator" />
+        /// </summary>
+        private string GetRenderedObjectSeparator(LogEventInfo logEvent)
+        {
+            return logEvent != null ? _objectSeparatorLayout.Render(logEvent) : ObjectSeparator;
+        }
+
+        /// <summary>
+        /// Append the quoted name and value separated by a colon
+        /// </summary>
+        private static bool AppendJsonProperty(StringBuilder builder, string name, string value, bool includePropertySeparator)
+        {
+            if (!string.IsNullOrEmpty(value))
+            {
+                if (includePropertySeparator)
+                {
+                    builder.Append(',');
+                }
+                AppendQuoted(builder, name);
+                builder.Append(':');
+                AppendQuoted(builder, value);
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Append the quoted name and value separated by a value separator
+        /// and ended by item separator
+        /// </summary>
+        private static bool AppendFlatProperty(
+            StringBuilder builder,
+            string name,
+            string value,
+            string valueSeparator,
+            string itemSeparator)
+        {
+            if (!string.IsNullOrEmpty(value))
+            {
+                builder.Append(itemSeparator);
+                builder.Append(name);
+                builder.Append(valueSeparator);
+                builder.Append(value);
+                return true;
+            }
+            return false;
         }
 
 #if NET46
