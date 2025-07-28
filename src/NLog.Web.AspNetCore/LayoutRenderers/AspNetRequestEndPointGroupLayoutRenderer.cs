8aa0ea1d3454ec33fb31adfaddd43b8c9d35647e
Rolf Kristensen <snakefoot@users.noreply.github.com>
2025-07-28 10:45:41 +0200

    aspnet-request-endpoint-group - EndPoint Metadata Group DisplayName (#1101)

---
+++ src/NLog.Web.AspNetCore/LayoutRenderers/AspNetRequestEndPointGroupLayoutRenderer.cs
@@ -0,0 +1,30 @@
+﻿#if NETCOREAPP3_0_OR_GREATER
+
+using System;
+using System.Text;
+using NLog.LayoutRenderers;
+using NLog.Web.Internal;
+
+namespace NLog.Web.LayoutRenderers
+{
+    /// <summary>
+    /// ASP.NET request EndPoint Group DisplayName. Metadata-attribute assigned by WithGroupName or [EndpointGroupName]
+    /// </summary>
+    /// <remarks>
+    /// <code>${aspnet-request-endpoint-group}</code>
+    /// </remarks>
+    /// <seealso href="https://github.com/NLog/NLog/wiki/AspNet-Request-EndPoint-Group-Layout-Renderer">Documentation on NLog Wiki</seealso>
+    [LayoutRenderer("aspnet-request-endpoint-group")]
+    public class AspNetRequestEndPointGroupLayoutRenderer : AspNetLayoutRendererBase
+    {
+        /// <inheritdoc/>
+        protected override void Append(StringBuilder builder, LogEventInfo logEvent)
+        {
+            var endPoint = HttpContextAccessor?.HttpContext.TryGetFeature<Microsoft.AspNetCore.Http.Features.IEndpointFeature>()?.Endpoint;
+            var endPointGroup = endPoint?.Metadata.GetMetadata<Microsoft.AspNetCore.Routing.EndpointGroupNameAttribute>();
+            builder.Append(endPointGroup?.EndpointGroupName);
+        }
+    }
+}
+
+#endif
