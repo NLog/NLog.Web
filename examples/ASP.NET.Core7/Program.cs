2dc4fc055a32374342a7f3d9e79235e9963bc89a
Rolf Kristensen <sweaty1@hotmail.com>
2026-08-08 02:20:19 +0200

    Updated ASP.NET Core Examples to match tutorial

--- examples/ASP.NET.Core7/Program.cs
+++ examples/ASP.NET.Core7/Program.cs
@@ -1,54 +1,37 @@
 using NLog;
 using NLog.Web;
 
-var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+var builder = WebApplication.CreateBuilder(args);
 
-try
+// Setup NLog for Logging
+await using var nlogFactory = NLog.LogManager.LogFactory;   // async flush/shutdown on dispose
+builder.Logging.ClearProviders();
+builder.Host.UseNLog();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+var summaries = new[]
 {
-    var builder = WebApplication.CreateBuilder(args);
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
 
-    // Add NLog for Logging
-    builder.Logging.ClearProviders();
-    builder.Host.UseNLog();
-
-    // Add services to the container.
-
-    var app = builder.Build();
-
-    // Configure the HTTP request pipeline.
-
-    var summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
-    app.MapGet("/weatherforecast", (ILogger<WeatherForecast> logger) =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-            new WeatherForecast
-            (
-                DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                Random.Shared.Next(-20, 55),
-                summaries[Random.Shared.Next(summaries.Length)]
-            ))
-            .ToArray();
-        logger.LogInformation("Generated {WeatherForecast}", forecast);
-        return forecast;
-    });
-
-    app.Run();
-}
-catch (Exception exception)
+app.MapGet("/weatherforecast", (ILogger<WeatherForecast> logger) =>
 {
-    // NLog: catch setup errors
-    logger.Error(exception, "Stopped program because of exception");
-    throw;
-}
-finally
-{
-    // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
-    NLog.LogManager.Shutdown();
-}
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    logger.LogInformation("Generated {WeatherForecast}", forecast);
+    return forecast;
+});
+
+app.Run();
 
 internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
